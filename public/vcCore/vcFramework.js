d82612c56123440f1263d6683431f7dedea3eaf7
java110 <928255095@qq.com>
2020-03-03 21:02:03 +0800

    第一次提交

---
+++ public/vcCore/vcFramework.js
@@ -0,0 +1,32 @@
+/**
+ * vcFramework
+ * 
+ * @author 吴学文
+ * 
+ * @version 0.0.1
+ * 
+ * @description 完成组件化编程思想
+ * 
+ */
+(function(){
+
+    var vcFramework = window.vcFramework ||{};
+
+    /**
+     * 从当前 HTML中找是否存在 <vc:create name="xxxx"></vc:create> 标签
+     */
+    findVcLabel = function(_currentHtml){
+
+    };
+
+    createElement = function(_html){
+
+
+
+    };
+
+    reader = function(){
+
+    };
+
+})();
