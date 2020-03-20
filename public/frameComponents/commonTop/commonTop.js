fda5819f5992f8bdd7bd6a5f3d39435a4991d706
java110 <928255095@qq.com>
2020-03-20 19:21:42 +0800

    加入百度统计

---
+++ public/frameComponents/commonTop/commonTop.js
@@ -0,0 +1,14 @@
+console.log('############################################################');
+console.log('#   HC小区项目开源于https://gitee.com/wuxw7/MicroCommunity  #');
+console.log('#   HC智慧家园项目开源于https://gitee.com/wuxw7/MicroCommunity  #');
+console.log('#   欢迎star                                               #');
+console.log('#   java110团队宣                                          #');
+console.log('############################################################');
+
+var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?adedae9bc250561cc17e96dc1fb46079";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
