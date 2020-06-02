3a5088b7126935e68309c85166e9b014352a8f57
java110 <928255095@qq.com>
2020-06-02 10:02:19 +0800

    证件照片只支持

--- public/components/common/company-cerdentials/company-cerdentials.js
+++ public/components/common/company-cerdentials/company-cerdentials.js
@@ -3,72 +3,72 @@
 
 **/
 
-(function(vc){
+(function (vc) {
     vc.extends({
-        data:{
-            companyCerdentialsInfo:{
-                value:"",
-                validityPeriod:""
+        data: {
+            companyCerdentialsInfo: {
+                value: "",
+                validityPeriod: ""
             }
         },
-         _initMethod:function(){
-             vc.component._companyinitDate();
-         },
-         _initEvent:function(){
-                // 证件信息
-               vc.component.$on('positivePhotoEvent',function(positivePhoto){
-                    for(var positivePhotoKey in positivePhoto){
-                         vc.component.companyCerdentialsInfo[positivePhotoKey] = positivePhoto[positivePhotoKey];
-                     }
-                      vc.component.$emit('companyCerdentialsEvent',vc.component.companyCerdentialsInfo);
-                });
-
-         },
-        watch:{
-            companyCerdentialsInfo:{
-                deep: true,
-                handler:function(){
-                    vc.component.$emit('companyCerdentialsEvent',vc.component.companyCerdentialsInfo);
-                }
-             }
+        _initMethod: function () {
+            vc.component._companyinitDate();
         },
-        methods:{
-            validateCerdentials:function(){
-                        if(!vc.component.validatePositivePhoto()){
-                            return false;
+        _initEvent: function () {
+            // 证件信息
+            vc.component.$on('positivePhotoEvent', function (positivePhoto) {
+                for (var positivePhotoKey in positivePhoto) {
+                    vc.component.companyCerdentialsInfo[positivePhotoKey] = positivePhoto[positivePhotoKey];
+                }
+                vc.component.$emit('companyCerdentialsEvent', vc.component.companyCerdentialsInfo);
+            });
+
+        },
+        watch: {
+            companyCerdentialsInfo: {
+                deep: true,
+                handler: function () {
+                    vc.component.$emit('companyCerdentialsEvent', vc.component.companyCerdentialsInfo);
+                }
+            }
+        },
+        methods: {
+            validateCerdentials: function () {
+                if (!vc.component.validatePositivePhoto()) {
+                    return false;
+                }
+                return vc.validate.validate({
+                    companyCerdentialsInfo: vc.component.companyCerdentialsInfo
+                }, {
+                    'companyCerdentialsInfo.value': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "证件号码不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "50",
+                            errInfo: "证件号码长度必须在50位之内"
+                        },
+                    ],
+
+                    'companyCerdentialsInfo.validityPeriod': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "成立时间不能为空"
+                        },
+                        {
+                            limit: "date",
+                            param: "",
+                            errInfo: "不是有效的日期，例如：2019-03-29"
                         }
-                        return vc.validate.validate({
-                                                               companyCerdentialsInfo:vc.component.companyCerdentialsInfo
-                                                           },{
-                                                               'companyCerdentialsInfo.value':[
-                                                                   {
-                                                                       limit:"required",
-                                                                       param:"",
-                                                                       errInfo:"证件号码不能为空"
-                                                                   },
-                                                                   {
-                                                                       limit:"maxLength",
-                                                                       param:"50",
-                                                                       errInfo:"证件号码长度必须在50位之内"
-                                                                   },
-                                                               ],
+                    ]
 
-                                                               'companyCerdentialsInfo.validityPeriod':[
-                                                                   {
-                                                                       limit:"required",
-                                                                       param:"",
-                                                                       errInfo:"成立时间不能为空"
-                                                                   },
-                                                                   {
-                                                                       limit:"date",
-                                                                       param:"",
-                                                                       errInfo:"不是有效的日期，例如：2019-03-29"
-                                                                   }
-                                                               ]
-
-                                                           });
-                    },
-            _companyinitDate:function(){
+                });
+            },
+            _companyinitDate: function () {
                 $('.company_time').datetimepicker({
                     language: 'zh-CN',
                     format: 'yyyy-mm-dd',
