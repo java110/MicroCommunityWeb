8808ac814ab84dd0c246592b2ea6f166a52a4020
java110 <928255095@qq.com>
2020-06-03 22:51:22 +0800

    优化编辑

--- public/pages/dev/jobManage/jobManage.js
+++ public/pages/dev/jobManage/jobManage.js
@@ -1,71 +1,128 @@
 /**
     入驻小区
 **/
-(function(vc){
+(function (vc) {
     var DEFAULT_PAGE = 1;
     var DEFAULT_ROWS = 10;
     vc.extends({
-        data:{
-            jobManageInfo:{
-                jobs:[],
-                name:'',
-                total:0,
-                records:1,
-                conditions:{
-                    taskId:'',
-                    taskName:'',
-                    templateId:''
+        data: {
+            jobManageInfo: {
+                jobs: [],
+                name: '',
+                total: 0,
+                records: 1,
+                conditions: {
+                    taskId: '',
+                    taskName: '',
+                    templateId: ''
                 }
             }
         },
-        _initMethod:function(){
+        _initMethod: function () {
             vc.component._listJobs(DEFAULT_PAGE, DEFAULT_ROWS);
         },
-        _initEvent:function(){
-            vc.on('jobManage','listJob',function(_param){
-                  vc.component._listJobs(DEFAULT_PAGE, DEFAULT_ROWS);
+        _initEvent: function () {
+            vc.on('jobManage', 'listJob', function (_param) {
+                vc.component._listJobs(DEFAULT_PAGE, DEFAULT_ROWS);
             });
-             vc.on('pagination','page_event',function(_currentPage){
-                vc.component._listJobs(_currentPage,DEFAULT_ROWS);
+            vc.on('pagination', 'page_event', function (_currentPage) {
+                vc.component._listJobs(_currentPage, DEFAULT_ROWS);
             });
         },
-        methods:{
-            _listJobs:function(_page, _rows){
+        methods: {
+            _listJobs: function (_page, _rows) {
                 vc.component.jobManageInfo.conditions.page = _page;
                 vc.component.jobManageInfo.conditions.row = _rows;
                 var param = {
-                    params:vc.component.jobManageInfo.conditions
-                 };
+                    params: vc.component.jobManageInfo.conditions
+                };
 
-               //发送get请求
-               vc.http.apiGet('task.listTasks',
-                             param,
-                             function(json,res){
-                                var _jobManageInfo=JSON.parse(json);
-                                vc.component.jobManageInfo.total = _jobManageInfo.total;
-                                vc.component.jobManageInfo.records = _jobManageInfo.records;
-                                vc.component.jobManageInfo.jobs = _jobManageInfo.data;
-                                vc.emit('pagination','init',{
-                                     total:vc.component.jobManageInfo.records,
-                                     currentPage:_page
-                                 });
-                             },function(errInfo,error){
-                                console.log('请求失败处理');
-                             }
-                           );
+                //发送get请求
+                vc.http.apiGet('task.listTasks',
+                    param,
+                    function (json, res) {
+                        var _jobManageInfo = JSON.parse(json);
+                        vc.component.jobManageInfo.total = _jobManageInfo.total;
+                        vc.component.jobManageInfo.records = _jobManageInfo.records;
+                        vc.component.jobManageInfo.jobs = _jobManageInfo.data;
+                        vc.emit('pagination', 'init', {
+                            total: vc.component.jobManageInfo.records,
+                            currentPage: _page
+                        });
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
             },
-            _openAddJobModal:function(){
-                vc.emit('addJob','openAddJobModal',{});
+            _openAddJobModal: function () {
+                vc.emit('addJob', 'openAddJobModal', {});
             },
-            _openEditJobModel:function(_job){
-                vc.emit('editJob','openEditJobModal',_job);
+            _openEditJobModel: function (_job) {
+                vc.emit('editJob', 'openEditJobModal', _job);
             },
-            _openDeleteJobModel:function(_job){
-                vc.emit('deleteJob','openDeleteJobModal',{
-                    taskId:_job.taskId
+            _openDeleteJobModel: function (_job) {
+                vc.emit('deleteJob', 'openDeleteJobModal', {
+                    taskId: _job.taskId
                 });
             },
-            _queryJobMethod:function(){
+            _openStartJob: function (_job) {
+                let param = {
+                    taskId: _job.taskId
+                };
+                vc.http.apiPost(
+                    'task.startTask',
+                    JSON.stringify(param),
+                    {
+                        emulateJSON: true
+                    },
+                    function (json, res) {
+                        let _json = JSON.parse(json);
+                        if (_json.code == 200) {
+                            //关闭model
+                            vc.emit('jobManage', 'listJob', {});
+                            vc.toast(_json.msg);
+                            return;
+                        }
+                        vc.toast(json);
+
+                    },
+                    function (errInfo, error) {
+                        console.log('请求失败处理');
+
+                        vc.toast(errInfo);
+
+                    });
+            },
+
+            _openStopJob: function (_job) {
+                let param = {
+                    taskId: _job.taskId
+                };
+                vc.http.apiPost(
+                    'task.stopTask',
+                    JSON.stringify(param),
+                    {
+                        emulateJSON: true
+                    },
+                    function (json, res) {
+                        let _json = JSON.parse(json);
+                        if (_json.code == 200) {
+                            //关闭model
+                            vc.emit('jobManage', 'listJob', {});
+                            vc.toast(_json.msg);
+                            return;
+                        }
+                        vc.toast(json);
+
+                    },
+                    function (errInfo, error) {
+                        console.log('请求失败处理');
+
+                        vc.toast(errInfo);
+
+                    });
+            },
+            _queryJobMethod: function () {
                 vc.component._listJobs(DEFAULT_PAGE, DEFAULT_ROWS);
 
             }
