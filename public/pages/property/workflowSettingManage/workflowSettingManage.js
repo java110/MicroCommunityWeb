dde07be43edfc9bd4a9cc495dde993c0e92266b2
java110 <928255095@qq.com>
2020-06-20 17:36:55 +0800

    优化流程页面

--- public/pages/property/workflowSettingManage/workflowSettingManage.js
+++ public/pages/property/workflowSettingManage/workflowSettingManage.js
@@ -17,11 +17,7 @@
         },
         methods: {
             saveWorkflowSettingInfo: function () {
-                if (!vc.component.addWorkflowSettingValidate()) {
-                    vc.toast(vc.validate.errInfo);
 
-                    return;
-                }
 
                 vc.component.workflowSettingInfo.communityId = vc.getCurrentCommunity().communityId;
 
@@ -35,7 +31,7 @@
                         let data = JSON.parse(json);
                         if (data.code == 0) {
                             vc.toast(data.msg);
-                            $that.getBack();
+                            $that._goBack();
                             return;
                         }
                         vc.toast(data.msg);
@@ -57,6 +53,76 @@
                 }
                 $that.workflowSettingInfo.flowId = flowId;
                 $that.workflowSettingInfo.flowName = vc.getParam('flowName');
+
+                //查询步骤
+                var param = {
+                    params: {
+                        communityId: vc.getCurrentCommunity().communityId,
+                        flowId: flowId
+                    }
+                };
+
+                //发送get请求
+                vc.http.apiGet('workflow.listWorkflowSteps',
+                    param,
+                    function (json, res) {
+                        var _workflowInfo = JSON.parse(json);
+                        if (_workflowInfo.code != '0') {
+                            vc.toast(_workflowInfo.msg);
+                            return;
+                        }
+                        $that._freshResStep(_workflowInfo.data);
+
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
+            },
+            _freshResStep: function (_data) {
+                $that.workflowSettingInfo.describle = _data.describle;
+                let _steps = [];
+                if (!_data.hasOwnProperty("workflowSteps")) {
+                    return;
+                }
+
+                for (let stepIndex = 0; stepIndex < _data.workflowSteps.length; stepIndex++) {
+                    let _workflowStep = _data.workflowSteps[stepIndex];
+                    let _step = {
+                        seq: stepIndex,
+                        staffId: _workflowStep.workflowStepStaffs[0].staffId,
+                        staffName: _workflowStep.workflowStepStaffs[0].staffName,
+                        type: _workflowStep.type,
+                        subStaff: []
+                    };
+                    if (_workflowStep.type == 2) {
+                        _steps.push(_step);
+                        continue;
+                    }
+                    let _subStaffs = [];
+
+                    if (!_workflowStep.hasOwnProperty("workflowStepStaffs") || _workflowStep.workflowStepStaffs.length < 2) {
+                        _steps.push(_step);
+                        continue;
+                    }
+
+                    for (let _subIndex = 1; _subIndex < _workflowStep.workflowStepStaffs.length; _subIndex++) {
+                        let _workflowStepStaff = _workflowStep.workflowStepStaffs[_subIndex];
+                        let _subStaff = {
+                            id: vc.uuid(),
+                            staffId: _workflowStepStaff.staffId,
+                            staffName: _workflowStepStaff.staffName
+                        }
+
+                        _subStaffs.push(_subStaff);
+                    }
+
+                    _step.subStaff = _subStaffs;
+                    _steps.push(_step);
+
+                }
+
+                $that.workflowSettingInfo.steps = _steps;
+
             },
             addWorkflowStep: function () {
                 let _step = {
@@ -70,7 +136,7 @@
             },
             chooseStaff: function (item) {
                 console.log(item);
-                vc.emit('selectStaff','openStaff',item);
+                vc.emit('selectStaff', 'openStaff', item);
             },
             _goBack: function () {
                 vc.getBack();
@@ -96,8 +162,8 @@
                     }
                 }
             },
-            chooseType:function(_item){
-                if(_item.type == '1'){
+            chooseType: function (_item) {
+                if (_item.type == '1') {
                     _item.subStaff = [];
                 }
             }
