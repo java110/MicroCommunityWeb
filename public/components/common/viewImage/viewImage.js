d60841df0cef353ec7c674fa8ba209fb7973ad07
java110 <928255095@qq.com>
2020-06-21 16:20:28 +0800

    优化 图片展示

--- public/components/common/viewImage/viewImage.js
+++ public/components/common/viewImage/viewImage.js
@@ -4,7 +4,7 @@
         data: {
             viewImageInfo: {
                 url: '',
-                showImage:false
+                showImage: false
             }
         },
         _initMethod: function () {
@@ -13,14 +13,52 @@
             vc.on('viewImage', 'showImage', function (_param) {
                 $that.viewImageInfo.url = _param.url;
                 $that.viewImageInfo.showImage = true;
+
+                $that._launchIntoFullscreen();
             });
         },
         methods: {
-            
-            _closeImage:function(){
+
+            _closeImage: function () {
+                $that._exitFullscreen();
                 $that.viewImageInfo.showImage = false;
-             }
+
+            },
+            _launchIntoFullscreen: function () {
+                let full = document.getElementById("viewImage");
+                console.log(full);
+                if (full.requestFullscreen) {
+                    full.requestFullscreen();
+                }
+                else if (full.mozRequestFullScreen) {
+                    full.mozRequestFullScreen();
+                }
+                else if (full.webkitRequestFullscreen) {
+                    full.webkitRequestFullscreen();
+                }
+                else if (full.msRequestFullscreen) {
+                    full.msRequestFullscreen();
+                }
+            },
+            _exitFullscreen: function () {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                }
+            },
         }
 
     });
+  
+
+    document.addEventListener("fullscreenchange", function (e) {
+        if (document.fullscreenElement) {
+            console.log('进入全屏')
+        } else {
+            $that.viewImageInfo.showImage = false;
+        }
+    })
 })(window.vc);
