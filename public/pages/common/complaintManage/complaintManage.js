e799eca8dbfbefa8e656a26336fb01b844b55c16
java110 <928255095@qq.com>
2020-06-21 19:51:39 +0800

    优化投诉界面

--- public/pages/common/complaintManage/complaintManage.js
+++ public/pages/common/complaintManage/complaintManage.js
@@ -86,6 +86,31 @@
                 } else {
                     vc.component.complaintManageInfo.moreCondition = true;
                 }
+            },
+            _openRunWorkflowImage:function(_complaint){
+                var param = {
+                    params: {
+                        communityId: vc.getCurrentCommunity().communityId,
+                        businessKey: _complaint.complaintId
+                    }
+                };
+                //发送get请求
+                vc.http.apiGet('workflow.listRunWorkflowImage',
+                    param,
+                    function (json, res) {
+                        var _workflowManageInfo = JSON.parse(json);
+                        if(_workflowManageInfo.code != '0'){
+                            vc.toast(_workflowManageInfo.msg);
+
+                            return ;
+                        }
+                        vc.emit('viewImage', 'showImage', {
+                            url: 'data:image/png;base64,'+_workflowManageInfo.data
+                        });
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
             }
 
 
