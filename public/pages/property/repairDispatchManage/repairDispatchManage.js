935eb4ea99f9f6adc4b7cb67af3e2b498cc84242
java110 <928255095@qq.com>
2020-06-30 19:16:18 +0800

    优化派单池页面

--- public/pages/property/repairDispatchManage/repairDispatchManage.js
+++ public/pages/property/repairDispatchManage/repairDispatchManage.js
@@ -99,7 +99,7 @@
                                 var _repairDispatchManageInfo=JSON.parse(json);
                                 vc.component.repairDispatchManageInfo.total = _repairDispatchManageInfo.total;
                                 vc.component.repairDispatchManageInfo.records = _repairDispatchManageInfo.records;
-                                vc.component.repairDispatchManageInfo.ownerRepairs = _repairDispatchManageInfo.ownerRepairs;
+                                vc.component.repairDispatchManageInfo.ownerRepairs = _repairDispatchManageInfo.data;
                                 vc.emit('pagination','init',{
                                      total:vc.component.repairDispatchManageInfo.records,
                                      currentPage:_page
