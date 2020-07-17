051578b55bd5fc808ee9bcc5bfd5069774b51b81
java110 <928255095@qq.com>
2020-07-17 10:11:25 +0800

    优化微信模板信息

--- public/components/property/deleteWechatSmsTemplate/deleteWechatSmsTemplate.js
+++ public/components/property/deleteWechatSmsTemplate/deleteWechatSmsTemplate.js
@@ -1,52 +1,51 @@
-(function(vc,vm){
+(function (vc, vm) {
 
     vc.extends({
-        data:{
-            deleteWechatSmsTemplateInfo:{
+        data: {
+            deleteWechatSmsTemplateInfo: {
 
             }
         },
-         _initMethod:function(){
+        _initMethod: function () {
 
-         },
-         _initEvent:function(){
-             vc.on('deleteWechatSmsTemplate','openDeleteWechatSmsTemplateModal',function(_params){
+        },
+        _initEvent: function () {
+            vc.on('deleteWechatSmsTemplate', 'openDeleteWechatSmsTemplateModal', function (_params) {
 
                 vc.component.deleteWechatSmsTemplateInfo = _params;
                 $('#deleteWechatSmsTemplateModel').modal('show');
 
             });
         },
-        methods:{
-            deleteWechatSmsTemplate:function(){
-                vc.component.deleteWechatSmsTemplateInfo.communityId=vc.getCurrentCommunity().communityId;
+        methods: {
+            deleteWechatSmsTemplate: function () {
+                vc.component.deleteWechatSmsTemplateInfo.communityId = vc.getCurrentCommunity().communityId;
                 vc.http.apiPost(
-                    'wechatSmsTemplate.deleteWechatSmsTemplate',
+                    '/wechat/deleteWechatTemplate',
                     JSON.stringify(vc.component.deleteWechatSmsTemplateInfo),
                     {
-                        emulateJSON:true
-                     },
-                     function(json,res){
-                        //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
+                        emulateJSON: true
+                    },
+                    function (json, res) {
                         let _json = JSON.parse(json);
                         if (_json.code == 0) {
                             //关闭model
                             $('#deleteWechatSmsTemplateModel').modal('hide');
-                            vc.emit('wechatSmsTemplateManage','listWechatSmsTemplate',{});
-                            return ;
+                            vc.emit('wechatSmsTemplateManage', 'listWechatSmsTemplate', {});
+                            return;
                         }
                         vc.message(_json.msg);
-                     },
-                     function(errInfo,error){
+                    },
+                    function (errInfo, error) {
                         console.log('请求失败处理');
                         vc.message(json);
 
-                     });
+                    });
             },
-            closeDeleteWechatSmsTemplateModel:function(){
+            closeDeleteWechatSmsTemplateModel: function () {
                 $('#deleteWechatSmsTemplateModel').modal('hide');
             }
         }
     });
 
-})(window.vc,window.vc.component);
+})(window.vc, window.vc.component);
--- public/pages/property/wechatSmsTemplateManage/wechatSmsTemplateManage.js
+++ public/pages/property/wechatSmsTemplateManage/wechatSmsTemplateManage.js
@@ -1,85 +1,86 @@
 /**
     入驻小区
 **/
-(function(vc){
+(function (vc) {
     var DEFAULT_PAGE = 1;
     var DEFAULT_ROWS = 10;
     vc.extends({
-        data:{
-            wechatSmsTemplateManageInfo:{
-                wechatSmsTemplates:[],
-                total:0,
-                records:1,
-                moreCondition:false,
-                templateId:'',
-                conditions:{
-                    smsTemplateId:'',
-templateType:'',
-templateId:'',
+        data: {
+            wechatSmsTemplateManageInfo: {
+                wechatSmsTemplates: [],
+                total: 0,
+                records: 1,
+                moreCondition: false,
+                templateId: '',
+                conditions: {
+                    smsTemplateId: '',
+                    templateType: '',
+                    templateId: '',
+                    communityId: vc.getCurrentCommunity().communityId
 
                 }
             }
         },
-        _initMethod:function(){
+        _initMethod: function () {
             vc.component._listWechatSmsTemplates(DEFAULT_PAGE, DEFAULT_ROWS);
         },
-        _initEvent:function(){
-            
-            vc.on('wechatSmsTemplateManage','listWechatSmsTemplate',function(_param){
-                  vc.component._listWechatSmsTemplates(DEFAULT_PAGE, DEFAULT_ROWS);
+        _initEvent: function () {
+
+            vc.on('wechatSmsTemplateManage', 'listWechatSmsTemplate', function (_param) {
+                vc.component._listWechatSmsTemplates(DEFAULT_PAGE, DEFAULT_ROWS);
             });
-             vc.on('pagination','page_event',function(_currentPage){
-                vc.component._listWechatSmsTemplates(_currentPage,DEFAULT_ROWS);
+            vc.on('pagination', 'page_event', function (_currentPage) {
+                vc.component._listWechatSmsTemplates(_currentPage, DEFAULT_ROWS);
             });
         },
-        methods:{
-            _listWechatSmsTemplates:function(_page, _rows){
+        methods: {
+            _listWechatSmsTemplates: function (_page, _rows) {
 
                 vc.component.wechatSmsTemplateManageInfo.conditions.page = _page;
                 vc.component.wechatSmsTemplateManageInfo.conditions.row = _rows;
                 var param = {
-                    params:vc.component.wechatSmsTemplateManageInfo.conditions
-               };
+                    params: vc.component.wechatSmsTemplateManageInfo.conditions
+                };
 
-               //发送get请求
-               vc.http.apiGet('wechatSmsTemplate.listWechatSmsTemplates',
-                             param,
-                             function(json,res){
-                                var _wechatSmsTemplateManageInfo=JSON.parse(json);
-                                vc.component.wechatSmsTemplateManageInfo.total = _wechatSmsTemplateManageInfo.total;
-                                vc.component.wechatSmsTemplateManageInfo.records = _wechatSmsTemplateManageInfo.records;
-                                vc.component.wechatSmsTemplateManageInfo.wechatSmsTemplates = _wechatSmsTemplateManageInfo.data;
-                                vc.emit('pagination','init',{
-                                     total:vc.component.wechatSmsTemplateManageInfo.records,
-                                     currentPage:_page
-                                 });
-                             },function(errInfo,error){
-                                console.log('请求失败处理');
-                             }
-                           );
+                //发送get请求
+                vc.http.apiGet('/wechat/queryWechatTemplate',
+                    param,
+                    function (json, res) {
+                        var _wechatSmsTemplateManageInfo = JSON.parse(json);
+                        vc.component.wechatSmsTemplateManageInfo.total = _wechatSmsTemplateManageInfo.total;
+                        vc.component.wechatSmsTemplateManageInfo.records = _wechatSmsTemplateManageInfo.records;
+                        vc.component.wechatSmsTemplateManageInfo.wechatSmsTemplates = _wechatSmsTemplateManageInfo.data;
+                        vc.emit('pagination', 'init', {
+                            total: vc.component.wechatSmsTemplateManageInfo.records,
+                            currentPage: _page
+                        });
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
             },
-            _openAddWechatSmsTemplateModal:function(){
-                vc.emit('addWechatSmsTemplate','openAddWechatSmsTemplateModal',{});
+            _openAddWechatSmsTemplateModal: function () {
+                vc.emit('addWechatSmsTemplate', 'openAddWechatSmsTemplateModal', {});
             },
-            _openEditWechatSmsTemplateModel:function(_wechatSmsTemplate){
-                vc.emit('editWechatSmsTemplate','openEditWechatSmsTemplateModal',_wechatSmsTemplate);
+            _openEditWechatSmsTemplateModel: function (_wechatSmsTemplate) {
+                vc.emit('editWechatSmsTemplate', 'openEditWechatSmsTemplateModal', _wechatSmsTemplate);
             },
-            _openDeleteWechatSmsTemplateModel:function(_wechatSmsTemplate){
-                vc.emit('deleteWechatSmsTemplate','openDeleteWechatSmsTemplateModal',_wechatSmsTemplate);
+            _openDeleteWechatSmsTemplateModel: function (_wechatSmsTemplate) {
+                vc.emit('deleteWechatSmsTemplate', 'openDeleteWechatSmsTemplateModal', _wechatSmsTemplate);
             },
-            _queryWechatSmsTemplateMethod:function(){
+            _queryWechatSmsTemplateMethod: function () {
                 vc.component._listWechatSmsTemplates(DEFAULT_PAGE, DEFAULT_ROWS);
 
             },
-            _moreCondition:function(){
-                if(vc.component.wechatSmsTemplateManageInfo.moreCondition){
+            _moreCondition: function () {
+                if (vc.component.wechatSmsTemplateManageInfo.moreCondition) {
                     vc.component.wechatSmsTemplateManageInfo.moreCondition = false;
-                }else{
+                } else {
                     vc.component.wechatSmsTemplateManageInfo.moreCondition = true;
                 }
             }
 
-             
+
         }
     });
 })(window.vc);
