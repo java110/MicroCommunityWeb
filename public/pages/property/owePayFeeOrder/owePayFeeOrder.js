d082bc2b16f0f38cdaf83f726342d4147e9cd3b8
java110 <928255095@qq.com>
2020-08-04 21:53:17 +0800

    优化代码

--- public/pages/property/owePayFeeOrder/owePayFeeOrder.js
+++ public/pages/property/owePayFeeOrder/owePayFeeOrder.js
@@ -98,7 +98,8 @@
                     },
                     function (json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
-                        if (res.status == 200) {
+                        let _json = JSON.parse(json);
+                        if (_json.code == 0) {
                             //关闭model
                             $("#payFeeResult").modal({
                                 backdrop: "static",//点击空白处不关闭对话框
@@ -106,7 +107,7 @@
                             });
                             return;
                         }
-                        vc.toast(json);
+                        vc.toast(_json.msg);
                     },
                     function (errInfo, error) {
                         console.log('请求失败处理');
