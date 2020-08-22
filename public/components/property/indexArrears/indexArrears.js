0f2bab1d1f5652aa22eb571d0ff1e96619d91ba5
java110 <928255095@qq.com>
2020-08-22 14:32:45 +0800

    优化代码

--- public/components/property/indexArrears/indexArrears.js
+++ public/components/property/indexArrears/indexArrears.js
@@ -1,65 +1,107 @@
-(function(vc){
+(function (vc) {
     var DEFAULT_PAGE = 1;
     var DEFAULT_ROWS = 10;
     vc.extends({
-        data:{
-            indexArrearsInfo:{
-                arrears:[],
-                feeTypeCd:'888800010001',
-                total:0,
-                records:1
+        data: {
+            indexArrearsInfo: {
+                complaintCount: 0,
+                repairCount: 0,
+                purchaseCount: 0,
+                collectionCount: 0
             }
         },
-        _initMethod:function(){
-            vc.component._listArrearsData(DEFAULT_PAGE,DEFAULT_ROWS);
+        _initMethod: function () {
+            vc.component._listCompaintOrders();
+            $that._listRepairCount();
+            $that._listPurchaseCount();
+            $that._listCollectionCount();
         },
-        _initEvent:function(){
-            vc.on("indexArrears","_listArrearsData",function(){
-                vc.component._listArrearsData(DEFAULT_PAGE,DEFAULT_ROWS);
-            });
-            vc.on('pagination','page_event',function(_currentPage){
-                vc.component._listArrearsData(_currentPage,DEFAULT_ROWS);
-            });
+        _initEvent: function () {
+
         },
-        methods:{
-            _listArrearsData:function(_page,_row){
-                if(vc.getCurrentCommunity() == null || vc.getCurrentCommunity == undefined){
-                    return ;
-                }
-                var param = {
-                    params:{
-                        page:_page,
-                        row:_row,
-                        communityId:vc.getCurrentCommunity().communityId,
-                        feeTypeCd:vc.component.indexArrearsInfo.feeTypeCd
+        methods: {
+            _listCompaintOrders: function () {
+                let param = {
+                    params: {
+                        page: 1,
+                        row: 10,
+                        communityId: vc.getCurrentCommunity().communityId
                     }
-                }
+                };
 
-               //发送get请求
-               vc.http.get('listArrears',
-                            'list',
-                             param,
-                             function(json,res){
-                                var listArrearsData =JSON.parse(json);
-
-                                vc.component.indexArrearsInfo.total = listArrearsData.total;
-                                vc.component.indexArrearsInfo.records = listArrearsData.records;
-                                vc.component.indexArrearsInfo.arrears = listArrearsData.arrears;
-
-                                vc.emit('pagination','init',{
-                                    total:vc.component.indexArrearsInfo.records,
-                                    currentPage:_page
-                                });
-                             },function(errInfo,error){
-                                console.log('请求失败处理');
-                             }
-                           );
-
+                //发送get请求
+                vc.http.get('myAuditComplaints',
+                    'list',
+                    param,
+                    function (json, res) {
+                        var _myAuditComplaintsInfo = JSON.parse(json);
+                        vc.component.indexArrearsInfo.complaintCount = _myAuditComplaintsInfo.total;
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
             },
-            _switchFeeType:function(_feeTypeCd){
-                console.log('_feeTypeCd')
-                vc.component.indexArrearsInfo.feeTypeCd = _feeTypeCd;
-                vc.component._listArrearsData(DEFAULT_PAGE,DEFAULT_ROWS);
+            _listRepairCount: function () {
+                let param = {
+                    params: {
+                        page: 1,
+                        row: 10,
+                        communityId: vc.getCurrentCommunity().communityId
+                    }
+                };
+
+                //发送get请求
+                vc.http.apiGet('ownerRepair.listStaffRepairs',
+                    param,
+                    function (json, res) {
+                        var _repairDispatchManageInfo = JSON.parse(json);
+                        vc.component.indexArrearsInfo.repairCount = _repairDispatchManageInfo.total;
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
+            },
+            _listPurchaseCount: function () {
+
+                let param = {
+                    params: {
+                        page: 1,
+                        row: 10,
+                        communityId: vc.getCurrentCommunity().communityId
+                    }
+                };
+
+                //发送get请求
+                vc.http.get('myAuditOrders',
+                    'list',
+                    param,
+                    function (json, res) {
+                        var _auditOrdersInfo = JSON.parse(json);
+                        vc.component.indexArrearsInfo.purchaseCount = _auditOrdersInfo.total;
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
+            },
+            _listCollectionCount: function (_page, _rows) {
+
+                let param = {
+                    params: {
+                        page: 1,
+                        row: 10,
+                        communityId: vc.getCurrentCommunity().communityId
+                    }
+                };
+                //发送get请求
+                vc.http.apiGet('/collection/getCollectionAuditOrder',
+                    param,
+                    function (json, res) {
+                        let _auditOrdersInfo = JSON.parse(json);
+                        vc.component.indexArrearsInfo.collectionCount = _auditOrdersInfo.total;
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
             }
         }
     })
