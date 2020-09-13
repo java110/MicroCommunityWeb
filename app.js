9374f129a4cf0bcdb3802484c9f828167c3a7cee
java110 <928255095@qq.com>
2020-09-14 00:00:07 +0800

    优化代码

--- app.js
+++ app.js
@@ -35,11 +35,9 @@ let opts = {
 //app.use('/callComponent',proxy('http://api.demo.winqi.cn:8012/',opts));
 //app.use('/callComponent',proxy('http://api.demo.winqi.cn:8012',opts));
 
-app.use('/callComponent',proxy('http://localhost:8012',opts));
-app.use('/app',proxy('http://localhost:8012',opts));
 
-// app.use('/callComponent',proxy('http://api.demo.winqi.cn:8012',opts));
-// app.use('/app',proxy('http://api.demo.winqi.cn:8012',opts));
+ app.use('/callComponent',proxy('http://api.demo.winqi.cn:8012',opts));
+ app.use('/app',proxy('http://api.demo.winqi.cn:8012',opts));
 
 
 //app.use('/callComponent',proxy('http://192.168.1.16:8012',opts));
