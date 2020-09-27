6b248524cc00cc39a25cc7390f27070936ce2c62
java110 <928255095@qq.com>
2020-09-27 08:44:40 +0800

    优化代码

--- public/vcCore/vcFramework.js
+++ public/vcCore/vcFramework.js
@@ -1897,66 +1897,68 @@ vc 校验 工具类 -method
             if (!(/(^\d{15}$)|(^\d{17}([0-9]|X)$)/.test(num))) {
                 return false;
             }
+
+            return  true;
             //校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。
             //下面分别分析出生日期和校验位
-            let len, re;
-            len = num.length;
-            if (len == 15) {
-                re = new RegExp(/^(\d{6})(\d{2})(\d{2})(\d{2})(\d{3})$/);
-                let arrSplit = num.match(re);
+            // let len, re;
+            // len = num.length;
+            // if (len == 15) {
+            //     re = new RegExp(/^(\d{6})(\d{2})(\d{2})(\d{2})(\d{3})$/);
+            //     let arrSplit = num.match(re);
 
-                //检查生日日期是否正确
-                let dtmBirth = new Date('19' + arrSplit[2] + '/' + arrSplit[3] + '/' + arrSplit[4]);
-                let bGoodDay;
-                bGoodDay = (dtmBirth.getYear() == Number(arrSplit[2])) && ((dtmBirth.getMonth() + 1) == Number(arrSplit[3])) && (dtmBirth.getDate() == Number(arrSplit[4]));
-                if (!bGoodDay) {
-                    return false;
-                }
-                else {
-                    //将15位身份证转成18位
-                    //校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。
-                    let arrInt = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
-                    let arrCh = new Array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
-                    let nTemp = 0, i;
-                    num = num.substr(0, 6) + '19' + num.substr(6, num.length - 6);
-                    for (i = 0; i < 17; i++) {
-                        nTemp += num.substr(i, 1) * arrInt[i];
-                    }
-                    num += arrCh[nTemp % 11];
-                    return true;
-                }
-            }
-            if (len == 18) {
-                re = new RegExp(/^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/);
-                let arrSplit = num.match(re);
+            //     //检查生日日期是否正确
+            //     let dtmBirth = new Date('19' + arrSplit[2] + '/' + arrSplit[3] + '/' + arrSplit[4]);
+            //     let bGoodDay;
+            //     bGoodDay = (dtmBirth.getYear() == Number(arrSplit[2])) && ((dtmBirth.getMonth() + 1) == Number(arrSplit[3])) && (dtmBirth.getDate() == Number(arrSplit[4]));
+            //     if (!bGoodDay) {
+            //         return false;
+            //     }
+            //     else {
+            //         //将15位身份证转成18位
+            //         //校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。
+            //         let arrInt = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+            //         let arrCh = new Array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+            //         let nTemp = 0, i;
+            //         num = num.substr(0, 6) + '19' + num.substr(6, num.length - 6);
+            //         for (i = 0; i < 17; i++) {
+            //             nTemp += num.substr(i, 1) * arrInt[i];
+            //         }
+            //         num += arrCh[nTemp % 11];
+            //         return true;
+            //     }
+            // }
+            // if (len == 18) {
+            //     re = new RegExp(/^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/);
+            //     let arrSplit = num.match(re);
 
-                //检查生日日期是否正确
-                let dtmBirth = new Date(arrSplit[2] + "/" + arrSplit[3] + "/" + arrSplit[4]);
-                let bGoodDay;
-                bGoodDay = (dtmBirth.getFullYear() == Number(arrSplit[2])) && ((dtmBirth.getMonth() + 1) == Number(arrSplit[3])) && (dtmBirth.getDate() == Number(arrSplit[4]));
-                if (!bGoodDay) {
-                    // alert(dtmBirth.getYear());
-                    //  alert(arrSplit[2]);
-                    return false;
-                }
-                else {
-                    //检验18位身份证的校验码是否正确。
-                    //校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。
-                    let valnum;
-                    let arrInt = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
-                    let arrCh = new Array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
-                    let nTemp = 0, i;
-                    for (i = 0; i < 17; i++) {
-                        nTemp += num.substr(i, 1) * arrInt[i];
-                    }
-                    valnum = arrCh[nTemp % 11];
-                    if (valnum != num.substr(17, 1)) {
-                        return false;
-                    }
-                    return true;
-                }
-            }
-            return false;
+            //     //检查生日日期是否正确
+            //     let dtmBirth = new Date(arrSplit[2] + "/" + arrSplit[3] + "/" + arrSplit[4]);
+            //     let bGoodDay;
+            //     bGoodDay = (dtmBirth.getFullYear() == Number(arrSplit[2])) && ((dtmBirth.getMonth() + 1) == Number(arrSplit[3])) && (dtmBirth.getDate() == Number(arrSplit[4]));
+            //     if (!bGoodDay) {
+            //         // alert(dtmBirth.getYear());
+            //         //  alert(arrSplit[2]);
+            //         return false;
+            //     }
+            //     else {
+            //         //检验18位身份证的校验码是否正确。
+            //         //校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。
+            //         let valnum;
+            //         let arrInt = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+            //         let arrCh = new Array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+            //         let nTemp = 0, i;
+            //         for (i = 0; i < 17; i++) {
+            //             nTemp += num.substr(i, 1) * arrInt[i];
+            //         }
+            //         valnum = arrCh[nTemp % 11];
+            //         if (valnum != num.substr(17, 1)) {
+            //             return false;
+            //         }
+            //         return true;
+            //     }
+            // }
+            // return false;
         }
 
     };
