110c17c01752951219d3f7f6fc8258355c26a6d0
java110 <928255095@qq.com>
2020-10-19 15:56:29 +0800

    优化代码

--- public/bigScreen/js/box5.js
+++ public/bigScreen/js/box5.js
@@ -33,13 +33,54 @@
         for (let _inIndex = 0; _inIndex < _dataArr.length; _inIndex++) {
             _li += "<li>" + (_inIndex + 1) + ".0、" + _dataArr[_inIndex].msg + "</li>";
 
-            if(_inIndex >=7){
+            if(_inIndex >=5){
                 break;
             }
         }
         _todayInpection.innerHTML = _li;
     }
 
+    function initPrePayment(_dataArr) {
+        let _todayInpection = document.getElementById("todayPreFee");
+        let _li = "<li>预交费提醒:</li>";
+        for (let _inIndex = 0; _inIndex < _dataArr.length; _inIndex++) {
+            _li += "<li>" + (_inIndex + 1) + ".0、" + _dataArr[_inIndex].feeName +  _dataArr[_inIndex].objCount +"户</li>";
+
+            if(_inIndex >=5){
+                break;
+            }
+        }
+        _todayInpection.innerHTML = _li;
+    }
+
+    function _loadPrePaymentCount(params) {
+        let param = {
+            params: {
+                communityId: vc.getCurrentCommunity().communityId
+            }
+        }
+        vc.http.apiGet(
+            '/reportFeeMonthStatistics/queryPrePaymentCount',
+            param,
+            function (json, res) {
+                //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
+                let _json = JSON.parse(json);
+                if (_json.code == 0) {
+                    let _data = _json.data;
+                    initPrePayment(_data);
+                    return;
+                }
+            },
+            function (errInfo, error) {
+                console.log('请求失败处理');
+
+                vc.toast(errInfo);
+
+            });
+    }
+
 
     _loadAssetInspection();
+
+    _loadPrePaymentCount();
 })()
---
+++ public/pages/property/reportPrePaymentFee/reportPrePaymentFee.js
@@ -0,0 +1,58 @@
+/**
+ 入驻小区
+ **/
+(function (vc) {
+    var DEFAULT_PAGE = 1;
+    var DEFAULT_ROWS = 10;
+    vc.extends({
+        data: {
+            reportPrePaymentFeeInfo: {
+                fees: [],
+                total: 0,
+                records: 1,
+                moreCondition: false,
+                title: '',
+            }
+        },
+        _initMethod: function () {
+            vc.component._listFees(DEFAULT_PAGE, DEFAULT_ROWS);
+        },
+        _initEvent: function () {
+            vc.on('pagination', 'page_event', function (_currentPage) {
+                vc.component._listFees(_currentPage, DEFAULT_ROWS);
+            });
+        },
+        methods: {
+            _queryMethod: function () {
+                vc.component._listFees(DEFAULT_PAGE, DEFAULT_ROWS);
+            },
+            _listFees: function (_page, _rows) {
+
+                var param = {
+                    params: {
+                        page: 1,
+                        row: 10,
+                        communityId: vc.getCurrentCommunity().communityId
+                    }
+                };
+
+                //发送get请求
+                vc.http.apiGet('/reportFeeMonthStatistics/queryPrePayment',
+                    param,
+                    function (json, res) {
+                        var _reportPrePaymentFeeInfo = JSON.parse(json);
+                        vc.component.reportPrePaymentFeeInfo.total = _reportPrePaymentFeeInfo.total;
+                        vc.component.reportPrePaymentFeeInfo.records = _reportPrePaymentFeeInfo.records;
+                        vc.component.reportPrePaymentFeeInfo.fees = _reportPrePaymentFeeInfo.data;
+                        vc.emit('pagination', 'init', {
+                            total: vc.component.reportPrePaymentFeeInfo.records,
+                            currentPage: _page
+                        });
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
+            }
+        }
+    });
+})(window.vc);
