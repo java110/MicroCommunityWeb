d23d1f9efc47e703704b408f8f2986479bedf42e
java110 <928255095@qq.com>
2020-11-05 14:57:42 +0800

    优化费用

--- public/components/property/editFeeConfig/editFeeConfig.js
+++ public/components/property/editFeeConfig/editFeeConfig.js
@@ -254,7 +254,8 @@
                     additionalAmount: '',
                     isDefault:'',
                     paymentCycle:'',
-                    paymentCd:''
+                    paymentCd:'',
+                    billType:''
                 };
                 vc.component.editFeeConfigInfo.feeTypeCds = _feeTypeCds;
                 vc.component.editFeeConfigInfo.computingFormulas = _computingFormulas;
