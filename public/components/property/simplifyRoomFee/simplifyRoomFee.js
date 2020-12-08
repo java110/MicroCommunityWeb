08b331988d3722dc99dfee046414e4331a03504b
java110 <928255095@qq.com>
2020-12-08 17:12:18 +0800

    加入 房屋费用 查询条件

--- public/components/property/simplifyRoomFee/simplifyRoomFee.js
+++ public/components/property/simplifyRoomFee/simplifyRoomFee.js
@@ -8,13 +8,17 @@
         data: {
             simplifyRoomFeeInfo: {
                 fees: [],
-                roomId:'',
-                roomName:'',
-                name:'',
-                roomName:'',
-                floorNum:'',
-                unitNum:'',
-                roomNum:'',
+                roomId: '',
+                roomName: '',
+                name: '',
+                roomName: '',
+                floorNum: '',
+                unitNum: '',
+                roomNum: '',
+                configId: '',
+                feeTypeCds: [],
+                feeTypeCd: '',
+                state: '2008001'
             }
         },
         _initMethod: function () {
@@ -23,32 +27,38 @@
         _initEvent: function () {
             //切换 至费用页面
             vc.on('simplifyRoomFee', 'switch', function (_param) {
-                if(_param.roomId == ''){
-                    return ;
+                if (_param.roomId == '') {
+                    return;
                 }
                 $that.clearSimplifyRoomFeeInfo();
-                vc.copyObject(_param,$that.simplifyRoomFeeInfo)
-                $that._listSimplifyRoomFee(DEFAULT_PAGE,DEFAULT_ROWS);
+                vc.copyObject(_param, $that.simplifyRoomFeeInfo)
+                $that._listSimplifyRoomFee(DEFAULT_PAGE, DEFAULT_ROWS);
+
+                vc.getDict('pay_fee_config', "fee_type_cd", function (_data) {
+                    $that.simplifyRoomFeeInfo.feeTypeCds = _data;
+                });
             });
 
             vc.on('simplifyRoomFee', 'notify', function () {
-                $that._listSimplifyRoomFee(DEFAULT_PAGE,DEFAULT_ROWS);
+                $that._listSimplifyRoomFee(DEFAULT_PAGE, DEFAULT_ROWS);
             });
-            
 
-            vc.on('simplifyRoomFee','paginationPlus', 'page_event',
-            function(_currentPage) {
-                vc.component._listSimplifyRoomFee(_currentPage, DEFAULT_ROWS);
-            });
+
+            vc.on('simplifyRoomFee', 'paginationPlus', 'page_event',
+                function (_currentPage) {
+                    vc.component._listSimplifyRoomFee(_currentPage, DEFAULT_ROWS);
+                });
         },
         methods: {
-            _listSimplifyRoomFee: function (_page,_row) {
+            _listSimplifyRoomFee: function (_page, _row) {
                 let param = {
                     params: {
                         page: _page,
                         row: _row,
                         communityId: vc.getCurrentCommunity().communityId,
-                        payerObjId: $that.simplifyRoomFeeInfo.roomId
+                        payerObjId: $that.simplifyRoomFeeInfo.roomId,
+                        configId: $that.simplifyRoomFeeInfo.configId,
+                        state: $that.simplifyRoomFeeInfo.state
                     }
                 };
 
@@ -61,7 +71,7 @@
                         vc.component.simplifyRoomFeeInfo.total = _feeConfigInfo.total;
                         vc.component.simplifyRoomFeeInfo.records = _feeConfigInfo.records;
                         vc.component.simplifyRoomFeeInfo.fees = _feeConfigInfo.fees;
-                        vc.emit('simplifyRoomFee','paginationPlus', 'init', {
+                        vc.emit('simplifyRoomFee', 'paginationPlus', 'init', {
                             total: _feeConfigInfo.records,
                             currentPage: _page
                         });
@@ -70,94 +80,124 @@
                     }
                 );
             },
-            _toOwnerPayFee:function(){
-                vc.jumpToPage('/admin.html#/pages/property/owePayFeeOrder?payObjId='+$that.simplifyRoomFeeInfo.roomId+"&payObjType=3333&roomName="+$that.simplifyRoomFeeInfo.roomName);
+            _toOwnerPayFee: function () {
+                vc.jumpToPage('/admin.html#/pages/property/owePayFeeOrder?payObjId=' + $that.simplifyRoomFeeInfo.roomId + "&payObjType=3333&roomName=" + $that.simplifyRoomFeeInfo.roomName);
             },
-            _openRoomCreateFeeAddModal:function(){
-                vc.emit('roomCreateFeeAdd', 'openRoomCreateFeeAddModal',{
-                    isMore:false,
-                    room:$that.simplifyRoomFeeInfo,
-                    ownerName:$that.simplifyRoomFeeInfo.name
+            _openRoomCreateFeeAddModal: function () {
+                vc.emit('roomCreateFeeAdd', 'openRoomCreateFeeAddModal', {
+                    isMore: false,
+                    room: $that.simplifyRoomFeeInfo,
+                    ownerName: $that.simplifyRoomFeeInfo.name
                 });
             },
             _openAddMeterWaterModal: function () {
                 vc.emit('addMeterWater', 'openAddMeterWaterModal', {
-                    roomId:$that.simplifyRoomFeeInfo.roomId,
-                    roomName:$that.simplifyRoomFeeInfo.roomName,
-                    ownerName:$that.simplifyRoomFeeInfo.name
-                    
+                    roomId: $that.simplifyRoomFeeInfo.roomId,
+                    roomName: $that.simplifyRoomFeeInfo.roomName,
+                    ownerName: $that.simplifyRoomFeeInfo.name
+
                 });
             },
-            _getAttrValue:function(_attrs,_specCd){
+            _getAttrValue: function (_attrs, _specCd) {
                 let _value = "";
                 _attrs.forEach(item => {
-                    if(item.specCd == _specCd){
+                    if (item.specCd == _specCd) {
                         _value = item.value;
-                        return ;
+                        return;
                     }
                 });
 
                 return _value;
             },
-            _getDeadlineTime:function(_fee){
+            _getDeadlineTime: function (_fee) {
 
-                if(_fee.amountOwed == 0 && _fee.endTime == _fee.deadlineTime){
+                if (_fee.amountOwed == 0 && _fee.endTime == _fee.deadlineTime) {
                     return "-";
                 }
 
-                if(_fee.state == '2009001'){
+                if (_fee.state == '2009001') {
                     return "-";
                 }
 
                 return _fee.deadlineTime;
             },
-            _getEndTime:function(_fee){
-                if(_fee.state == '2009001'){
+            _getEndTime: function (_fee) {
+                if (_fee.state == '2009001') {
                     return "-";
                 }
                 return _fee.endTime;
             },
-            _openProxyFeeModal:function(){ //创建代收费用
+            _openProxyFeeModal: function () { //创建代收费用
                 vc.emit('addProxyFee', 'openAddProxyFeeModal', {
-                    roomId:$that.simplifyRoomFeeInfo.roomId,
-                    roomName:$that.simplifyRoomFeeInfo.roomName,
-                    ownerName:$that.simplifyRoomFeeInfo.name
+                    roomId: $that.simplifyRoomFeeInfo.roomId,
+                    roomName: $that.simplifyRoomFeeInfo.roomName,
+                    ownerName: $that.simplifyRoomFeeInfo.name
                 });
             },
-            _payFee:function(_fee){
-                _fee.roomName=$that.simplifyRoomFeeInfo.roomName;
-                _fee.builtUpArea=$that.simplifyRoomFeeInfo.builtUpArea;
-               //vc.jumpToPage('/admin.html#/pages/property/payFeeOrder?'+vc.objToGetParam(_fee));
-               vc.jumpToPage('/admin.html#/pages/property/payFeeOrder?feeId='+_fee.feeId);
+            _payFee: function (_fee) {
+                _fee.roomName = $that.simplifyRoomFeeInfo.roomName;
+                _fee.builtUpArea = $that.simplifyRoomFeeInfo.builtUpArea;
+                //vc.jumpToPage('/admin.html#/pages/property/payFeeOrder?'+vc.objToGetParam(_fee));
+                vc.jumpToPage('/admin.html#/pages/property/payFeeOrder?feeId=' + _fee.feeId);
             },
-            _editFee:function(_fee){
-                vc.emit('editFee', 'openEditFeeModal',_fee);
+            _editFee: function (_fee) {
+                vc.emit('editFee', 'openEditFeeModal', _fee);
             },
-            _payFeeHis:function(_fee){
-                _fee.builtUpArea=$that.simplifyRoomFeeInfo.builtUpArea;
-               vc.jumpToPage('/admin.html#/pages/property/propertyFee?'+vc.objToGetParam(_fee));
+            _payFeeHis: function (_fee) {
+                _fee.builtUpArea = $that.simplifyRoomFeeInfo.builtUpArea;
+                vc.jumpToPage('/admin.html#/pages/property/propertyFee?' + vc.objToGetParam(_fee));
             },
-            _deleteFee:function(_fee){
+            _deleteFee: function (_fee) {
 
-                vc.emit('deleteFee','openDeleteFeeModal',{
-                         communityId:vc.getCurrentCommunity().communityId,
-                         feeId:_fee.feeId
+                vc.emit('deleteFee', 'openDeleteFeeModal', {
+                    communityId: vc.getCurrentCommunity().communityId,
+                    feeId: _fee.feeId
                 });
             },
-            clearSimplifyRoomFeeInfo:function(){
-                $that. simplifyRoomFeeInfo= {
+            clearSimplifyRoomFeeInfo: function () {
+                let _feeConfigs = $that.roomCreateFeeAddInfo.feeTypeCds;
+                $that.simplifyRoomFeeInfo = {
                     fees: [],
-                    roomId:'',
-                    roomName:'',
-                    name:'',
-                    roomName:'',
-                    floorNum:'',
-                    unitNum:'',
-                    roomNum:'',
+                    roomId: '',
+                    roomName: '',
+                    name: '',
+                    roomName: '',
+                    floorNum: '',
+                    unitNum: '',
+                    roomNum: '',
+                    feeConfigs: _feeConfigs,
+                    feeTypeCd: '',
+                    configId: '',
+                    state: '2008001'
                 }
+            },
+            _changeSimplifyRoomFeeFeeTypeCd: function (_feeTypeCd) {
+                $that.simplifyRoomFeeInfo.configId = '';
+                var param = {
+                    params: {
+                        page: 1,
+                        row: 50,
+                        communityId: vc.getCurrentCommunity().communityId,
+                        feeTypeCd: _feeTypeCd,
+                        isDefault: 'F',
+                        valid: '1'
+                    }
+                };
+                //发送get请求
+                vc.http.get('roomCreateFeeAdd', 'list', param,
+                    function (json, res) {
+                        var _feeConfigManageInfo = JSON.parse(json);
+                        vc.component.simplifyRoomFeeInfo.feeConfigs = _feeConfigManageInfo.feeConfigs;
+                    },
+                    function (errInfo, error) {
+                        console.log('请求失败处理');
+                    });
+            },
+            _changeSimplifyRoomConfigId: function () {
+                vc.emit('simplifyRoomFee', 'notify', {});
             }
 
-           
+
         }
     });
 })(window.vc);
