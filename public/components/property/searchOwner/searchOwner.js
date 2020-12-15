f7ff7deb390e1d8924c2861332f1499f306085e5
java110 <928255095@qq.com>
2020-12-15 11:36:05 +0800

    优化 海南物业公司 显示问题

--- public/components/property/searchOwner/searchOwner.js
+++ public/components/property/searchOwner/searchOwner.js
@@ -1,63 +1,63 @@
-(function(vc){
+(function (vc) {
     vc.extends({
         propTypes: {
-           emitChooseOwner:vc.propTypes.string,
-           emitLoadData:vc.propTypes.string
+            emitChooseOwner: vc.propTypes.string,
+            emitLoadData: vc.propTypes.string
         },
-        data:{
-            searchOwnerInfo:{
-                owners:[],
-                _currentOwnerName:'',
-                _currentOwnerId:''
+        data: {
+            searchOwnerInfo: {
+                owners: [],
+                _currentOwnerName: '',
+                roomName: ''
             }
         },
-        _initMethod:function(){
+        _initMethod: function () {
 
         },
-        _initEvent:function(){
-            vc.on('searchOwner','openSearchOwnerModel',function(_param){
+        _initEvent: function () {
+            vc.on('searchOwner', 'openSearchOwnerModel', function (_param) {
                 console.log("打开业主成员界面")
                 $('#searchOwnerModel').modal('show');
                 vc.component._refreshSearchOwnerData();
-                vc.component._loadAllOwnerInfo(1,10);
+                vc.component._loadAllOwnerInfo(1, 10);
             });
         },
-        methods:{
-            _loadAllOwnerInfo:function(_page,_row,_name){
+        methods: {
+            _loadAllOwnerInfo: function (_page, _row, _name) {
                 var param = {
-                    params:{
-                        page:_page,
-                        row:_row,
-                        communityId:vc.getCurrentCommunity().communityId,
-                        name:_name,
-                        ownerId:vc.component.searchOwnerInfo._currentOwnerId,
-                        ownerTypeCd:'1001'
+                    params: {
+                        page: _page,
+                        row: _row,
+                        communityId: vc.getCurrentCommunity().communityId,
+                        name: _name,
+                        roomName: vc.component.searchOwnerInfo.roomName,
+                        ownerTypeCd: '1001'
                     }
                 };
 
                 //发送get请求
-               vc.http.get('searchOwner',
-                            'listOwner',
-                             param,
-                             function(json){
-                                var _ownerInfo = JSON.parse(json);
-                                vc.component.searchOwnerInfo.owners = _ownerInfo.owners;
-                             },function(){
-                                console.log('请求失败处理');
-                             }
-                           );
+                vc.http.get('searchOwner',
+                    'listOwner',
+                    param,
+                    function (json) {
+                        var _ownerInfo = JSON.parse(json);
+                        vc.component.searchOwnerInfo.owners = _ownerInfo.owners;
+                    }, function () {
+                        console.log('请求失败处理');
+                    }
+                );
             },
-            chooseOwner:function(_owner){
-                vc.emit($props.emitChooseOwner,'chooseOwner',_owner);
-                vc.emit($props.emitLoadData,'listOwnerData',{
-                    ownerId:_owner.ownerId
+            chooseOwner: function (_owner) {
+                vc.emit($props.emitChooseOwner, 'chooseOwner', _owner);
+                vc.emit($props.emitLoadData, 'listOwnerData', {
+                    ownerId: _owner.ownerId
                 });
                 $('#searchOwnerModel').modal('hide');
             },
-            searchOwners:function(){
-                vc.component._loadAllOwnerInfo(1,10,vc.component.searchOwnerInfo._currentOwnerName);
+            searchOwners: function () {
+                vc.component._loadAllOwnerInfo(1, 10, vc.component.searchOwnerInfo._currentOwnerName);
             },
-            _refreshSearchOwnerData:function(){
+            _refreshSearchOwnerData: function () {
                 vc.component.searchOwnerInfo._currentOwnerName = "";
             }
         }
