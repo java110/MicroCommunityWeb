a485ec01a3d88fdab6eff49863a2df395693f210
java110 <928255095@qq.com>
2021-01-18 11:09:10 +0800

    优化代码

--- public/pages/property/tempCarFeeConfigManage/tempCarFeeConfigManage.js
+++ public/pages/property/tempCarFeeConfigManage/tempCarFeeConfigManage.js
@@ -16,6 +16,7 @@
                     feeName: '',
                     paId: '',
                     carType: '',
+                    communityId:vc.getCurrentCommunity().communityId
                 }
             }
         },
