a5c514bb63818438a0e1c1b06233365eb4c19a86
java110 <928255095@qq.com>
2021-02-05 17:32:48 +0800

    加入问卷信息

---
+++ public/components/property/addQuestionAnswer/addQuestionAnswer.js
@@ -0,0 +1,148 @@
+(function(vc){
+
+    vc.extends({
+        propTypes: {
+               callBackListener:vc.propTypes.string, //父组件名称
+               callBackFunction:vc.propTypes.string //父组件监听方法
+        },
+        data:{
+            addQuestionAnswerInfo:{
+                qaId:'',
+                qaType:'',
+qaName:'',
+startTime:'',
+endTime:'',
+remark:'',
+
+            }
+        },
+         _initMethod:function(){
+
+         },
+         _initEvent:function(){
+            vc.on('addQuestionAnswer','openAddQuestionAnswerModal',function(){
+                $('#addQuestionAnswerModel').modal('show');
+            });
+        },
+        methods:{
+            addQuestionAnswerValidate(){
+                return vc.validate.validate({
+                    addQuestionAnswerInfo:vc.component.addQuestionAnswerInfo
+                },{
+                    'addQuestionAnswerInfo.qaType':[
+{
+                            limit:"required",
+                            param:"",
+                            errInfo:"问卷类型不能为空"
+                        },
+ {
+                            limit:"num",
+                            param:"",
+                            errInfo:"问卷类型格式错误"
+                        },
+                    ],
+'addQuestionAnswerInfo.qaName':[
+{
+                            limit:"required",
+                            param:"",
+                            errInfo:"问卷名称不能为空"
+                        },
+ {
+                            limit:"maxLength",
+                            param:"256",
+                            errInfo:"问卷名称太长"
+                        },
+                    ],
+'addQuestionAnswerInfo.startTime':[
+{
+                            limit:"required",
+                            param:"",
+                            errInfo:"开始时间不能为空"
+                        },
+ {
+                            limit:"datetime",
+                            param:"",
+                            errInfo:"开始时间错误"
+                        },
+                    ],
+'addQuestionAnswerInfo.endTime':[
+{
+                            limit:"required",
+                            param:"",
+                            errInfo:"结束时间不能为空"
+                        },
+ {
+                            limit:"datetime",
+                            param:"",
+                            errInfo:"结束时间错误"
+                        },
+                    ],
+'addQuestionAnswerInfo.remark':[
+ {
+                            limit:"maxLength",
+                            param:"512",
+                            errInfo:"备注太长"
+                        },
+                    ],
+
+
+
+
+                });
+            },
+            saveQuestionAnswerInfo:function(){
+                if(!vc.component.addQuestionAnswerValidate()){
+                    vc.toast(vc.validate.errInfo);
+
+                    return ;
+                }
+
+                vc.component.addQuestionAnswerInfo.communityId = vc.getCurrentCommunity().communityId;
+                //不提交数据将数据 回调给侦听处理
+                if(vc.notNull($props.callBackListener)){
+                    vc.emit($props.callBackListener,$props.callBackFunction,vc.component.addQuestionAnswerInfo);
+                    $('#addQuestionAnswerModel').modal('hide');
+                    return ;
+                }
+
+                vc.http.apiPost(
+                    'questionAnswer.saveQuestionAnswer',
+                    JSON.stringify(vc.component.addQuestionAnswerInfo),
+                    {
+                        emulateJSON:true
+                     },
+                     function(json,res){
+                        //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
+                        let _json = JSON.parse(json);
+                        if (_json.code == 0) {
+                            //关闭model
+                            $('#addQuestionAnswerModel').modal('hide');
+                            vc.component.clearAddQuestionAnswerInfo();
+                            vc.emit('questionAnswerManage','listQuestionAnswer',{});
+
+                            return ;
+                        }
+                        vc.message(_json.msg);
+
+                     },
+                     function(errInfo,error){
+                        console.log('请求失败处理');
+
+                        vc.message(errInfo);
+
+                     });
+            },
+            clearAddQuestionAnswerInfo:function(){
+                vc.component.addQuestionAnswerInfo = {
+                                            qaType:'',
+qaName:'',
+startTime:'',
+endTime:'',
+remark:'',
+
+                                        };
+            }
+        }
+    });
+
+})(window.vc);
