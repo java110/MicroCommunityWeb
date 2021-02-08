42b184a4e21fdfc830dcf1a346bb9b2cf2a7e4f9
java110 <928255095@qq.com>
2021-02-08 10:44:11 +0800

    123

--- public/components/property/addActivitiesBeautifulStaff/addActivitiesBeautifulStaff.js
+++ public/components/property/addActivitiesBeautifulStaff/addActivitiesBeautifulStaff.js
@@ -12,15 +12,20 @@
                 staffId: '',
                 activitiesNum: '',
                 workContent: '',
-
+                activitiesRules: []
             }
         },
         _initMethod: function () {
-
+            $that._initAddActivitiesBeautifulStaffInfo();
         },
         _initEvent: function () {
             vc.on('addActivitiesBeautifulStaff', 'openAddActivitiesBeautifulStaffModal', function () {
-                $('#addActivitiesBeautifulStaffModel').modal('show');
+                $that._listAddActivitiesRules();
+            });
+            vc.on("addActivitiesBeautifulStaff", "notify", function (_param) {
+                if (_param.hasOwnProperty("staffId")) {
+                    vc.component.addActivitiesBeautifulStaffInfo.staffId = _param.staffId;
+                }
             });
         },
         methods: {
@@ -98,7 +103,7 @@
                 }
 
                 vc.http.apiPost(
-                    'activitiesBeautifulStaff.saveActivitiesBeautifulStaff',
+                    '/activitiesRule/saveActivitiesBeautifulStaff',
                     JSON.stringify(vc.component.addActivitiesBeautifulStaffInfo),
                     {
                         emulateJSON: true
@@ -130,9 +135,93 @@
                     staffId: '',
                     activitiesNum: '',
                     workContent: '',
-
+                    activitiesRules: []
                 };
-            }
+            },
+            _listAddActivitiesRules: function (_page, _rows) {
+                let _that = $that.addActivitiesBeautifulStaffInfo;
+                var param = {
+                    params: {
+                        page: 1,
+                        row: 50,
+                        communityId: vc.getCurrentCommunity().communityId
+                    }
+                };
+                //发送get请求
+                vc.http.apiGet('/activitiesRule/queryActivitiesRule',
+                    param,
+                    function (json, res) {
+                        var _activitiesRuleManageInfo = JSON.parse(json);
+                        _that.activitiesRules = _activitiesRuleManageInfo.data;
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
+            },
+            _closeAddActivitiesBeauifulStaffInfo: function () {
+                vc.emit('activitiesBeautifulStaffManage', 'listActivitiesBeautifulStaff', {});
+            },
+            sendFile: function ($summernote, files) {
+                console.log('上传图片', files);
+
+                var param = new FormData();
+                param.append("uploadFile", files[0]);
+                param.append('communityId', vc.getCurrentCommunity().communityId);
+
+                vc.http.upload(
+                    'addNoticeView',
+                    'uploadImage',
+                    param,
+                    {
+                        emulateJSON: true,
+                        //添加请求头
+                        headers: {
+                            "Content-Type": "multipart/form-data"
+                        }
+                    },
+                    function (json, res) {
+                        //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
+                        if (res.status == 200) {
+                            var data = JSON.parse(json);
+                            //关闭model
+                            $summernote.summernote('insertImage', "/callComponent/download/getFile/file?fileId=" + data.fileId + "&communityId=" + vc.getCurrentCommunity().communityId);
+                            return;
+                        }
+                        vc.toast(json);
+                    },
+                    function (errInfo, error) {
+                        console.log('请求失败处理');
+                        vc.toast(errInfo);
+                    });
+
+            },
+            _initAddActivitiesBeautifulStaffInfo: function () {
+                let $summernote = $('.summernote').summernote({
+                    lang: 'zh-CN',
+                    height: 300,
+                    placeholder: '必填，请输入工作简介',
+                    callbacks: {
+                        onImageUpload: function (files, editor, $editable) {
+                            vc.component.sendFile($summernote, files);
+                        },
+                        onChange: function (contents, $editable) {
+                            vc.component.addActivitiesBeautifulStaffInfo.workContent = contents;
+                        }
+                    },
+                    toolbar: [
+                        ['style', ['style']],
+                        ['font', ['bold', 'italic', 'underline', 'clear']],
+                        ['fontname', ['fontname']],
+                        ['color', ['color']],
+                        ['para', ['ul', 'ol', 'paragraph']],
+                        ['height', ['height']],
+                        ['table', ['table']],
+                        ['insert', ['link', 'picture']],
+                        ['view', ['fullscreen', 'codeview']],
+                        ['help', ['help']]
+                    ],
+                });
+            },
         }
     });
 
--- public/pages/property/activitiesBeautifulStaffManage/activitiesBeautifulStaffManage.js
+++ public/pages/property/activitiesBeautifulStaffManage/activitiesBeautifulStaffManage.js
@@ -1,85 +1,88 @@
 /**
     入驻小区
 **/
-(function(vc){
+(function (vc) {
     var DEFAULT_PAGE = 1;
     var DEFAULT_ROWS = 10;
     vc.extends({
-        data:{
-            activitiesBeautifulStaffManageInfo:{
-                activitiesBeautifulStaffs:[],
-                total:0,
-                records:1,
-                moreCondition:false,
-                beId:'',
-                conditions:{
-                    ruleId:'',
-staffName:'',
-activitiesNum:'',
+        data: {
+            activitiesBeautifulStaffManageInfo: {
+                activitiesBeautifulStaffs: [],
+                total: 0,
+                records: 1,
+                moreCondition: false,
+                componentShow: 'activitiesBeautifulStaffManage',
+                beId: '',
+                conditions: {
+                    ruleId: '',
+                    staffName: '',
+                    activitiesNum: '',
 
                 }
             }
         },
-        _initMethod:function(){
+        _initMethod: function () {
             vc.component._listActivitiesBeautifulStaffs(DEFAULT_PAGE, DEFAULT_ROWS);
         },
-        _initEvent:function(){
-            
-            vc.on('activitiesBeautifulStaffManage','listActivitiesBeautifulStaff',function(_param){
-                  vc.component._listActivitiesBeautifulStaffs(DEFAULT_PAGE, DEFAULT_ROWS);
+        _initEvent: function () {
+
+            vc.on('activitiesBeautifulStaffManage', 'listActivitiesBeautifulStaff', function (_param) {
+
+                vc.component._listActivitiesBeautifulStaffs(DEFAULT_PAGE, DEFAULT_ROWS);
             });
-             vc.on('pagination','page_event',function(_currentPage){
-                vc.component._listActivitiesBeautifulStaffs(_currentPage,DEFAULT_ROWS);
+            vc.on('pagination', 'page_event', function (_currentPage) {
+                vc.component._listActivitiesBeautifulStaffs(_currentPage, DEFAULT_ROWS);
             });
         },
-        methods:{
-            _listActivitiesBeautifulStaffs:function(_page, _rows){
-
+        methods: {
+            _listActivitiesBeautifulStaffs: function (_page, _rows) {
+                $that.activitiesBeautifulStaffManageInfo.componentShow = 'activitiesBeautifulStaffManage';
                 vc.component.activitiesBeautifulStaffManageInfo.conditions.page = _page;
                 vc.component.activitiesBeautifulStaffManageInfo.conditions.row = _rows;
                 var param = {
-                    params:vc.component.activitiesBeautifulStaffManageInfo.conditions
-               };
+                    params: vc.component.activitiesBeautifulStaffManageInfo.conditions
+                };
 
-               //发送get请求
-               vc.http.apiGet('activitiesBeautifulStaff.listActivitiesBeautifulStaffs',
-                             param,
-                             function(json,res){
-                                var _activitiesBeautifulStaffManageInfo=JSON.parse(json);
-                                vc.component.activitiesBeautifulStaffManageInfo.total = _activitiesBeautifulStaffManageInfo.total;
-                                vc.component.activitiesBeautifulStaffManageInfo.records = _activitiesBeautifulStaffManageInfo.records;
-                                vc.component.activitiesBeautifulStaffManageInfo.activitiesBeautifulStaffs = _activitiesBeautifulStaffManageInfo.data;
-                                vc.emit('pagination','init',{
-                                     total:vc.component.activitiesBeautifulStaffManageInfo.records,
-                                     currentPage:_page
-                                 });
-                             },function(errInfo,error){
-                                console.log('请求失败处理');
-                             }
-                           );
+                //发送get请求
+                vc.http.apiGet('/activitiesRule/queryActivitiesBeautifulStaff',
+                    param,
+                    function (json, res) {
+                        var _activitiesBeautifulStaffManageInfo = JSON.parse(json);
+                        vc.component.activitiesBeautifulStaffManageInfo.total = _activitiesBeautifulStaffManageInfo.total;
+                        vc.component.activitiesBeautifulStaffManageInfo.records = _activitiesBeautifulStaffManageInfo.records;
+                        vc.component.activitiesBeautifulStaffManageInfo.activitiesBeautifulStaffs = _activitiesBeautifulStaffManageInfo.data;
+                        vc.emit('pagination', 'init', {
+                            total: vc.component.activitiesBeautifulStaffManageInfo.records,
+                            currentPage: _page
+                        });
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
             },
-            _openAddActivitiesBeautifulStaffModal:function(){
-                vc.emit('addActivitiesBeautifulStaff','openAddActivitiesBeautifulStaffModal',{});
+            _openAddActivitiesBeautifulStaffModal: function () {
+                $that.activitiesBeautifulStaffManageInfo.componentShow = 'addActivitiesBeautifulStaff';
+                vc.emit('addActivitiesBeautifulStaff', 'openAddActivitiesBeautifulStaffModal', {});
             },
-            _openEditActivitiesBeautifulStaffModel:function(_activitiesBeautifulStaff){
-                vc.emit('editActivitiesBeautifulStaff','openEditActivitiesBeautifulStaffModal',_activitiesBeautifulStaff);
+            _openEditActivitiesBeautifulStaffModel: function (_activitiesBeautifulStaff) {
+                vc.emit('editActivitiesBeautifulStaff', 'openEditActivitiesBeautifulStaffModal', _activitiesBeautifulStaff);
             },
-            _openDeleteActivitiesBeautifulStaffModel:function(_activitiesBeautifulStaff){
-                vc.emit('deleteActivitiesBeautifulStaff','openDeleteActivitiesBeautifulStaffModal',_activitiesBeautifulStaff);
+            _openDeleteActivitiesBeautifulStaffModel: function (_activitiesBeautifulStaff) {
+                vc.emit('deleteActivitiesBeautifulStaff', 'openDeleteActivitiesBeautifulStaffModal', _activitiesBeautifulStaff);
             },
-            _queryActivitiesBeautifulStaffMethod:function(){
+            _queryActivitiesBeautifulStaffMethod: function () {
                 vc.component._listActivitiesBeautifulStaffs(DEFAULT_PAGE, DEFAULT_ROWS);
 
             },
-            _moreCondition:function(){
-                if(vc.component.activitiesBeautifulStaffManageInfo.moreCondition){
+            _moreCondition: function () {
+                if (vc.component.activitiesBeautifulStaffManageInfo.moreCondition) {
                     vc.component.activitiesBeautifulStaffManageInfo.moreCondition = false;
-                }else{
+                } else {
                     vc.component.activitiesBeautifulStaffManageInfo.moreCondition = true;
                 }
             }
 
-             
+
         }
     });
 })(window.vc);
