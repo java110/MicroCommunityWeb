ad77c13d88c124b7ce03e723c3b34e6be0b71b65
java110 <928255095@qq.com>
2021-04-14 01:41:04 +0800

    加入商品专区和专区商品

---
+++ public/components/admin/editMainCategoryProduct/editMainCategoryProduct.js
@@ -0,0 +1,123 @@
+(function (vc, vm) {
+
+    vc.extends({
+        data: {
+            editMainCategoryProductInfo: {
+                mcProductId: '',
+                startTime: '',
+                endTime: '',
+                seq: '',
+            }
+        },
+        _initMethod: function () {
+            vc.initDateTime('editStartTime', function (_value) {
+                $that.editMainCategoryProductInfo.startTime = _value;
+            });
+
+            vc.initDateTime('editEndTime', function (_value) {
+                $that.editMainCategoryProductInfo.endTime = _value;
+            });
+        },
+        _initEvent: function () {
+            vc.on('editMainCategoryProduct', 'openEditMainCategoryProductModal', function (_params) {
+                vc.component.refreshEditMainCategoryProductInfo();
+                $('#editMainCategoryProductModel').modal('show');
+                vc.copyObject(_params, vc.component.editMainCategoryProductInfo);
+                vc.component.editMainCategoryProductInfo.communityId = vc.getCurrentCommunity().communityId;
+            });
+        },
+        methods: {
+            editMainCategoryProductValidate: function () {
+                return vc.validate.validate({
+                    editMainCategoryProductInfo: vc.component.editMainCategoryProductInfo
+                }, {
+                    
+                    'editMainCategoryProductInfo.startTime': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "开始时间不能为空"
+                        },
+                        {
+                            limit: "dateTime",
+                            param: "",
+                            errInfo: "开始时间格式错误"
+                        },
+                    ],
+                    'editMainCategoryProductInfo.endTime': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "结束时间不能为空"
+                        },
+                        {
+                            limit: "dateTime",
+                            param: "",
+                            errInfo: "结束时间格式错误"
+                        },
+                    ],
+                    'editMainCategoryProductInfo.seq': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "排序不能为空"
+                        },
+                        {
+                            limit: "num",
+                            param: "",
+                            errInfo: "排序必须是整数"
+                        },
+                    ],
+                    'editMainCategoryProductInfo.mcProductId': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "主键不能为空"
+                        }]
+
+                });
+            },
+            editMainCategoryProduct: function () {
+                if (!vc.component.editMainCategoryProductValidate()) {
+                    vc.toast(vc.validate.errInfo);
+                    return;
+                }
+
+                vc.http.apiPost(
+                    '/productCategory/updateMainCategoryProduct',
+                    JSON.stringify(vc.component.editMainCategoryProductInfo),
+                    {
+                        emulateJSON: true
+                    },
+                    function (json, res) {
+                        //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
+                        let _json = JSON.parse(json);
+                        if (_json.code == 0) {
+                            //关闭model
+                            $('#editMainCategoryProductModel').modal('hide');
+                            vc.emit('mainCategoryProductManage', 'listMainCategoryProduct', {});
+                            return;
+                        }
+                        vc.message(_json.msg);
+                    },
+                    function (errInfo, error) {
+                        console.log('请求失败处理');
+
+                        vc.message(errInfo);
+                    });
+            },
+            refreshEditMainCategoryProductInfo: function () {
+                vc.component.editMainCategoryProductInfo = {
+                    mcProductId: '',
+                    mainCategoryId: '',
+                    productId: '',
+                    startTime: '',
+                    endTime: '',
+                    seq: '',
+
+                }
+            }
+        }
+    });
+
+})(window.vc, window.vc.component);
