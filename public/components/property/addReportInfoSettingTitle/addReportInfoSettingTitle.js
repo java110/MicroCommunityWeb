de8bb0d7de80dd472cf17b9fcd1a8d9149e61354
1098226878 <1098226878@qq.com>
2021-08-06 17:42:38 +0800

    完善疫情管控登记前端

---
+++ public/components/property/addReportInfoSettingTitle/addReportInfoSettingTitle.js
@@ -0,0 +1,164 @@
+(function (vc) {
+
+    vc.extends({
+        propTypes: {
+            callBackListener: vc.propTypes.string, //父组件名称
+            callBackFunction: vc.propTypes.string //父组件监听方法
+        },
+        data: {
+            addReportInfoSettingTitleInfo: {
+                titleId: '',
+                titleType: '',
+                title: '',
+                seq: '',
+                settingId: '',
+                titleValues: []
+            }
+        },
+        _initMethod: function () {
+
+        },
+        _initEvent: function () {
+            vc.on('addReportInfoSettingTitle', 'openAddReportInfoSettingTitleModal', function (_param) {
+                vc.copyObject(_param, $that.addReportInfoSettingTitleInfo);
+                $('#addReportInfoSettingTitleModel').modal('show');
+                console.log($that.addReportInfoSettingTitleInfo);
+            });
+        },
+        methods: {
+            addReportInfoSettingTitleValidate() {
+                return vc.validate.validate({
+                    addReportInfoSettingTitleInfo: vc.component.addReportInfoSettingTitleInfo
+                }, {
+                    'addReportInfoSettingTitleInfo.titleType': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "题目类型不能为空"
+                        },
+                        {
+                            limit: "num",
+                            param: "",
+                            errInfo: "题目类型格式错误"
+                        },
+                    ],
+                    'addReportInfoSettingTitleInfo.title': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "问卷题目不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "256",
+                            errInfo: "问卷题目太长"
+                        },
+                    ],
+                    'addReportInfoSettingTitleInfo.seq': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "顺序不能为空"
+                        },
+                        {
+                            limit: "num",
+                            param: "",
+                            errInfo: "顺序必须是数字"
+                        },
+                    ]
+                });
+            },
+            saveReportInfoSettingTitleInfo: function () {
+                if (!vc.component.addReportInfoSettingTitleValidate()) {
+                    vc.toast(vc.validate.errInfo);
+
+                    return;
+                }
+                vc.component.addReportInfoSettingTitleInfo.communityId = vc.getCurrentCommunity().communityId;
+                //不提交数据将数据 回调给侦听处理
+                if (vc.notNull($props.callBackListener)) {
+                    vc.emit($props.callBackListener, $props.callBackFunction, vc.component.addReportInfoSettingTitleInfo);
+                    $('#addReportInfoSettingTitleModel').modal('hide');
+                    return;
+                }
+                vc.http.apiPost(
+                    '/reportInfoSettingTitle/saveReportInfoSettingTitle',
+                    JSON.stringify(vc.component.addReportInfoSettingTitleInfo),
+                    {
+                        emulateJSON: true
+                    },
+                    function (json, res) {
+                        //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
+                        let _json = JSON.parse(json);
+                        if (_json.code == 0) {
+                            //关闭model
+                            $('#addReportInfoSettingTitleModel').modal('hide');
+                            vc.component.clearAddReportInfoSettingTitleInfo();
+                            vc.emit('reportInfoSettingTitleManage', 'listReportInfoSettingTitle', {});
+
+                            return;
+                        }
+                        vc.message(_json.msg);
+
+                    },
+                    function (errInfo, error) {
+                        console.log('请求失败处理');
+
+                        vc.message(errInfo);
+
+                    });
+            },
+            clearAddReportInfoSettingTitleInfo: function () {
+                vc.component.addReportInfoSettingTitleInfo = {
+                    titleId: '',
+                    titleType: '',
+                    title: '',
+                    seq: '',
+                    settingId: '',
+                    titleValues: []
+                };
+            },
+            _changeAddTitleType: function () {
+
+                let _titleType = $that.addReportInfoSettingTitleInfo.titleType;
+
+                if (_titleType == '3003') {
+                    $that.addReportInfoSettingTitleInfo.titleValues = [];
+                    return;
+                }
+
+                $that.addReportInfoSettingTitleInfo.titleValues = [
+                    {
+                        qaValue: '',
+                        seq: 1
+                    }
+                ];
+            },
+            _addTitleValue: function () {
+                $that.addReportInfoSettingTitleInfo.titleValues.push(
+                    {
+                        qaValue: '',
+                        seq: $that.addReportInfoSettingTitleInfo.titleValues.length + 1
+                    }
+                );
+            },
+            _deleteTitleValue: function (_seq) {
+                console.log(_seq);
+
+                let _newTitleValues = [];
+                let _tmpTitleValues = $that.addReportInfoSettingTitleInfo.titleValues;
+                _tmpTitleValues.forEach(item => {
+                    if (item.seq != _seq) {
+                        _newTitleValues.push({
+                            qaValue: item.qaValue,
+                            seq: _newTitleValues.length + 1
+                        })
+                    }
+                });
+
+                $that.addReportInfoSettingTitleInfo.titleValues = _newTitleValues;
+            }
+        }
+    });
+
+})(window.vc);
---
+++ public/pages/property/reportInfoSettingTitleManage/reportInfoSettingTitleManage.js
@@ -0,0 +1,109 @@
+/**
+    入驻小区
+**/
+(function (vc) {
+    var DEFAULT_PAGE = 1;
+    var DEFAULT_ROWS = 10;
+    vc.extends({
+        data: {
+            reportInfoSettingTitleManageInfo: {
+                reportInfoSettingTitles: [],
+                total: 0,
+                records: 1,
+                moreCondition: false,
+                titleId: '',
+                conditions: {
+                    titleType: '',
+                    title: '',
+                    titleId: '',
+                    settingid: ''
+
+                }
+            }
+        },
+        _initMethod: function () {
+            let _settingId = vc.getParam('settingId');
+            $that.reportInfoSettingTitleManageInfo.conditions.settingId = _settingId;
+            vc.component._listReportInfoSettingTitles(DEFAULT_PAGE, DEFAULT_ROWS);
+        },
+        _initEvent: function () {
+
+            vc.on('reportInfoSettingTitleManage', 'listreportInfoSettingTitle', function (_param) {
+                vc.component._listReportInfoSettingTitles(DEFAULT_PAGE, DEFAULT_ROWS);
+            });
+            vc.on('pagination', 'page_event', function (_currentPage) {
+                vc.component._listReportInfoSettingTitles(_currentPage, DEFAULT_ROWS);
+            });
+        },
+        methods: {
+            _listReportInfoSettingTitles: function (_page, _rows) {
+
+                vc.component.reportInfoSettingTitleManageInfo.conditions.page = _page;
+                vc.component.reportInfoSettingTitleManageInfo.conditions.row = _rows;
+                var param = {
+                    params: vc.component.reportInfoSettingTitleManageInfo.conditions
+                };
+
+                //发送get请求
+                vc.http.apiGet('/reportInfoSettingTitle/querySettingTitle',
+                    param,
+                    function (json, res) {
+                        var _reportInfoSettingTitleManageInfo = JSON.parse(json);
+                        vc.component.reportInfoSettingTitleManageInfo.total = _reportInfoSettingTitleManageInfo.total;
+                        vc.component.reportInfoSettingTitleManageInfo.records = _reportInfoSettingTitleManageInfo.records;
+                        vc.component.reportInfoSettingTitleManageInfo.reportInfoSettingTitles = _reportInfoSettingTitleManageInfo.data;
+                        vc.emit('pagination', 'init', {
+                            total: vc.component.reportInfoSettingTitleManageInfo.records,
+                            dataCount: vc.component.reportInfoSettingTitleManageInfo.total,
+                            currentPage: _page
+                        });
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
+            },
+            _openAddReportInfoSettingTitleModal: function () {
+                vc.emit('addReportInfoSettingTitle', 'openAddReportInfoSettingTitleModal', {
+                    settingId: $that.reportInfoSettingTitleManageInfo.conditions.settingId,
+                });
+            },
+            _openEditReportInfoSettingTitleModel: function (_reportInfoSettingTitle) {
+                vc.emit('editreportInfoSettingTitle', 'openEditreportInfoSettingTitleModal', _reportInfoSettingTitle);
+            },
+            _openDeleteReportInfoSettingTitleModel: function (_reportInfoSettingTitle) {
+                vc.emit('deletereportInfoSettingTitle', 'openDeletereportInfoSettingTitleModal', _reportInfoSettingTitle);
+            },
+            _queryReportInfoSettingTitleMethod: function () {
+                vc.component._listReportInfoSettingTitles(DEFAULT_PAGE, DEFAULT_ROWS);
+
+            },
+            _goBack: function () {
+                vc.goBack();
+            },
+            _moreCondition: function () {
+                if (vc.component.reportInfoSettingTitleManageInfo.moreCondition) {
+                    vc.component.reportInfoSettingTitleManageInfo.moreCondition = false;
+                } else {
+                    vc.component.reportInfoSettingTitleManageInfo.moreCondition = true;
+                }
+            },
+            _getTitleTypeName:function(_titleType){
+                if(_titleType == '1001'){
+                    return '单选';
+                }else if(_titleType == '2002'){
+                    return '多选';
+                }else{
+                    return '简答';
+                }
+            },
+            _openQuestionValueModel:function(_reportInfoSettingTitle){
+                vc.emit('questionValue','openQuestionValueModel',_reportInfoSettingTitle);
+            },
+            _toQuestionValueModel:function(_reportInfoSettingTitle){
+                vc.jumpToPage('/admin.html#/pages/property/reportInfoSettingTitleValueManage?titleId='+_reportInfoSettingTitle.titleId)
+            }
+
+
+        }
+    });
+})(window.vc);
