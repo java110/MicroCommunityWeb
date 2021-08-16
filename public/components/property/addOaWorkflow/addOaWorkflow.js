67858c82c4f4466d41f3a2457c056fdfa0f8ff12
java110 <928255095@qq.com>
2021-08-16 16:53:40 +0800

    加入流程实例

---
+++ public/components/property/addOaWorkflow/addOaWorkflow.js
@@ -0,0 +1,119 @@
+(function (vc) {
+
+    vc.extends({
+        propTypes: {
+            callBackListener: vc.propTypes.string, //父组件名称
+            callBackFunction: vc.propTypes.string //父组件监听方法
+        },
+        data: {
+            addOaWorkflowInfo: {
+                flowId: '',
+                flowName: '',
+                flowType: '',
+                describle: '',
+            }
+        },
+        _initMethod: function () {
+
+        },
+        _initEvent: function () {
+            vc.on('addOaWorkflow', 'openAddOaWorkflowModal', function () {
+                $('#addOaWorkflowModel').modal('show');
+            });
+        },
+        methods: {
+            addOaWorkflowValidate() {
+                return vc.validate.validate({
+                    addOaWorkflowInfo: vc.component.addOaWorkflowInfo
+                }, {
+                    'addOaWorkflowInfo.flowName': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "流程名称不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "64",
+                            errInfo: "流程名称超过64位"
+                        },
+                    ],
+                    'addOaWorkflowInfo.flowType': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "流程类型不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "12",
+                            errInfo: "流程类型不能为空"
+                        },
+                    ],
+                    'addOaWorkflowInfo.describle': [
+                        {
+                            limit: "maxLength",
+                            param: "200",
+                            errInfo: "备注内容不能超过200"
+                        },
+                    ],
+
+
+
+
+                });
+            },
+            saveOaWorkflowInfo: function () {
+                if (!vc.component.addOaWorkflowValidate()) {
+                    vc.toast(vc.validate.errInfo);
+
+                    return;
+                }
+
+                vc.component.addOaWorkflowInfo.communityId = vc.getCurrentCommunity().communityId;
+                //不提交数据将数据 回调给侦听处理
+                if (vc.notNull($props.callBackListener)) {
+                    vc.emit($props.callBackListener, $props.callBackFunction, vc.component.addOaWorkflowInfo);
+                    $('#addOaWorkflowModel').modal('hide');
+                    return;
+                }
+
+                vc.http.apiPost(
+                    'oaWorkflow.saveOaWorkflow',
+                    JSON.stringify(vc.component.addOaWorkflowInfo),
+                    {
+                        emulateJSON: true
+                    },
+                    function (json, res) {
+                        //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
+                        let _json = JSON.parse(json);
+                        if (_json.code == 0) {
+                            //关闭model
+                            $('#addOaWorkflowModel').modal('hide');
+                            vc.component.clearAddOaWorkflowInfo();
+                            vc.emit('oaWorkflowManage', 'listOaWorkflow', {});
+
+                            return;
+                        }
+                        vc.message(_json.msg);
+
+                    },
+                    function (errInfo, error) {
+                        console.log('请求失败处理');
+
+                        vc.message(errInfo);
+
+                    });
+            },
+            clearAddOaWorkflowInfo: function () {
+                vc.component.addOaWorkflowInfo = {
+                    flowName: '',
+                    flowType: '',
+                    describle: '',
+
+                };
+            }
+        }
+    });
+
+})(window.vc);
---
+++ public/pages/property/oaWorkflowManage/oaWorkflowManage.js
@@ -0,0 +1,84 @@
+/**
+    入驻小区
+**/
+(function (vc) {
+    var DEFAULT_PAGE = 1;
+    var DEFAULT_ROWS = 10;
+    vc.extends({
+        data: {
+            oaWorkflowManageInfo: {
+                oaWorkflows: [],
+                total: 0,
+                records: 1,
+                moreCondition: false,
+                flowId: '',
+                conditions: {
+                    flowName: '',
+                    describle: '',
+                    flowType: '',
+                }
+            }
+        },
+        _initMethod: function () {
+            vc.component._listOaWorkflows(DEFAULT_PAGE, DEFAULT_ROWS);
+        },
+        _initEvent: function () {
+
+            vc.on('oaWorkflowManage', 'listOaWorkflow', function (_param) {
+                vc.component._listOaWorkflows(DEFAULT_PAGE, DEFAULT_ROWS);
+            });
+            vc.on('pagination', 'page_event', function (_currentPage) {
+                vc.component._listOaWorkflows(_currentPage, DEFAULT_ROWS);
+            });
+        },
+        methods: {
+            _listOaWorkflows: function (_page, _rows) {
+
+                vc.component.oaWorkflowManageInfo.conditions.page = _page;
+                vc.component.oaWorkflowManageInfo.conditions.row = _rows;
+                var param = {
+                    params: vc.component.oaWorkflowManageInfo.conditions
+                };
+
+                //发送get请求
+                vc.http.apiGet('oaWorkflow.listOaWorkflows',
+                    param,
+                    function (json, res) {
+                        var _oaWorkflowManageInfo = JSON.parse(json);
+                        vc.component.oaWorkflowManageInfo.total = _oaWorkflowManageInfo.total;
+                        vc.component.oaWorkflowManageInfo.records = _oaWorkflowManageInfo.records;
+                        vc.component.oaWorkflowManageInfo.oaWorkflows = _oaWorkflowManageInfo.data;
+                        vc.emit('pagination', 'init', {
+                            total: vc.component.oaWorkflowManageInfo.records,
+                            currentPage: _page
+                        });
+                    }, function (errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
+            },
+            _openAddOaWorkflowModal: function () {
+                vc.emit('addOaWorkflow', 'openAddOaWorkflowModal', {});
+            },
+            _openEditOaWorkflowModel: function (_oaWorkflow) {
+                vc.emit('editOaWorkflow', 'openEditOaWorkflowModal', _oaWorkflow);
+            },
+            _openDeleteOaWorkflowModel: function (_oaWorkflow) {
+                vc.emit('deleteOaWorkflow', 'openDeleteOaWorkflowModal', _oaWorkflow);
+            },
+            _queryOaWorkflowMethod: function () {
+                vc.component._listOaWorkflows(DEFAULT_PAGE, DEFAULT_ROWS);
+
+            },
+            _moreCondition: function () {
+                if (vc.component.oaWorkflowManageInfo.moreCondition) {
+                    vc.component.oaWorkflowManageInfo.moreCondition = false;
+                } else {
+                    vc.component.oaWorkflowManageInfo.moreCondition = true;
+                }
+            }
+
+
+        }
+    });
+})(window.vc);
