639a6c5946144c1a914bcf7a2bebf666a49aac69
java110 <928255095@qq.com>
2021-10-13 08:57:20 +0800

    优化代码

--- public/pages/property/printPayFeeXinShiDai/printPayFeeXinShiDai.js
+++ public/pages/property/printPayFeeXinShiDai/printPayFeeXinShiDai.js
@@ -176,7 +176,8 @@
                     function (json, res) {
                         let _json = JSON.parse(json);
                         let ownerCar = _json.data[0];
-                        $that.printPayFeeInfo.feeReceipt[0].objName = ownerCar.roomName;
+                        let _roomName = ownerCar.roomName.replace('栋','-').replace("单元",'-').replace('室','');
+                        $that.printPayFeeInfo.feeReceipt[0].objName = _roomName;
                     }, function (errInfo, error) {
                         console.log('请求失败处理');
                     }
