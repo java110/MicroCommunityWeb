2bdd4e3691865e7c4c932dc375a446b719026a07
java110 <928255095@qq.com>
2021-10-16 01:53:01 +0800

    优化代码

--- public/components/property/parkingAreaControlInCar/parkingAreaControlInCar.js
+++ public/components/property/parkingAreaControlInCar/parkingAreaControlInCar.js
@@ -2,10 +2,13 @@
  入驻小区
  **/
 (function (vc) {
+    var DEFAULT_PAGE = 1;
+    var DEFAULT_ROWS = 10;
     vc.extends({
         data: {
             parkingAreaControlInCarInfo: {
-                carIns: []
+                carIns: [],
+                paId: ''
 
             }
         },
@@ -13,10 +16,42 @@
 
         },
         _initEvent: function () {
-
+            vc.on('parkingAreaControlInCar', 'switch', function (_data) {
+                $that.parkingAreaControlInCarInfo.paId = _data.paId;
+                $that._loadParkingAreaControlInCarData(DEFAULT_PAGE, DEFAULT_ROWS);
+            });
+            vc.on('parkingAreaControlInCar', 'paginationPlus', 'page_event',
+                function (_currentPage) {
+                    vc.component._loadParkingAreaControlInCarData(_currentPage, DEFAULT_ROWS);
+                });
         },
         methods: {
-
+            _loadParkingAreaControlInCarData: function (_page, _row) {
+                let param = {
+                    params: {
+                        page: _page,
+                        row: _row,
+                        communityId: vc.getCurrentCommunity().communityId,
+                        paId: $that.parkingAreaControlInCarInfo.paId,
+                    }
+                };
+                //发送get请求
+                vc.http.apiGet('/carInout.listCarInParkingAreaCmd',
+                    param,
+                    function (json) {
+                        let _feeConfigInfo = JSON.parse(json);
+                        vc.component.parkingAreaControlInCarInfo.total = _feeConfigInfo.total;
+                        vc.component.parkingAreaControlInCarInfo.records = _feeConfigInfo.records;
+                        vc.component.parkingAreaControlInCarInfo.carIns = _feeConfigInfo.data;
+                        vc.emit('parkingAreaControlInCar', 'paginationPlus', 'init', {
+                            total: _feeConfigInfo.records,
+                            currentPage: _page
+                        });
+                    }, function () {
+                        console.log('请求失败处理');
+                    }
+                );
+            }
         }
     });
 })(window.vc);
