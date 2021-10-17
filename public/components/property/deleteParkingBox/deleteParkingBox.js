5cbb8a22cf4f2ef9557db3d4acd4265999cbeec9
java110 <928255095@qq.com>
2021-10-18 01:11:23 +0800

    优化代码

---
+++ public/components/property/deleteParkingBox/deleteParkingBox.js
@@ -0,0 +1,52 @@
+(function(vc,vm){
+
+    vc.extends({
+        data:{
+            deleteParkingBoxInfo:{
+
+            }
+        },
+         _initMethod:function(){
+
+         },
+         _initEvent:function(){
+             vc.on('deleteParkingBox','openDeleteParkingBoxModal',function(_params){
+
+                vc.component.deleteParkingBoxInfo = _params;
+                $('#deleteParkingBoxModel').modal('show');
+
+            });
+        },
+        methods:{
+            deleteParkingBox:function(){
+                vc.component.deleteParkingBoxInfo.communityId=vc.getCurrentCommunity().communityId;
+                vc.http.apiPost(
+                    'parkingBox.deleteParkingBox',
+                    JSON.stringify(vc.component.deleteParkingBoxInfo),
+                    {
+                        emulateJSON:true
+                     },
+                     function(json,res){
+                        //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
+                        let _json = JSON.parse(json);
+                        if (_json.code == 0) {
+                            //关闭model
+                            $('#deleteParkingBoxModel').modal('hide');
+                            vc.emit('parkingBoxManage','listParkingBox',{});
+                            return ;
+                        }
+                        vc.message(_json.msg);
+                     },
+                     function(errInfo,error){
+                        console.log('请求失败处理');
+                        vc.message(json);
+
+                     });
+            },
+            closeDeleteParkingBoxModel:function(){
+                $('#deleteParkingBoxModel').modal('hide');
+            }
+        }
+    });
+
+})(window.vc,window.vc.component);
