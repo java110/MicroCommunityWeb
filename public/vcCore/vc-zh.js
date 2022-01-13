db150057c9d9a9e8255b28bb6ccdafc2cc72b77d
java110 <928255095@qq.com>
2022-01-13 12:21:33 +0800

    优化代码

--- public/vcCore/vc-zh.js
+++ public/vcCore/vc-zh.js
@@ -1,20 +1,20 @@
 /**
  * 英文包
  */
-(function (window) {
+(function(window) {
     window.lang = {
-        "systemName": "HC智慧物业管理系统",
+        "systemName": "HC小区管理系统",
         "systemSimpleName": "HC",
-        "subSystemName": "HC智慧物业管理系统",
-        "companyTeam": "java110研发团队",
-        "welcome": "欢迎访问HC智慧物业管理系统",
+        "subSystemName": "小区管理系统",
+        "companyTeam": "java110团队",
+        "welcome": "欢迎访问HC小区管理系统",
         "signOut": "退出",
-        "signIn":"登录",
-        "register":"注册",
-        "moreCommunity":"更多小区",
-        "moreMsg":"查看全部消息",
-        "title":"HC智慧物业管理系统",
-        "noAccount":"还没有账号？ ",
-        "areyouhasaccount":"已经有账户了？" 
+        "signIn": "登录",
+        "register": "注册",
+        "moreCommunity": "更多小区",
+        "moreMsg": "查看全部消息",
+        "title": "HC小区管理系统",
+        "noAccount": "还没有账号？ ",
+        "areyouhasaccount": "已经有账户了？"
     }
 })(window)
