4d031ba5ecbf162b4b96aece9cb781e8325ee74e
1098226878@qq.com <1098226878@qq.com>
2022-01-13 11:54:26 +0800

    优化代码

--- public/vcCore/vc-zh.js
+++ public/vcCore/vc-zh.js
@@ -3,17 +3,17 @@
  */
 (function (window) {
     window.lang = {
-        "systemName": "云尚智慧物业管理系统",
-        "systemSimpleName": "RS",
-        "subSystemName": "云尚智慧物业管理系统",
-        "companyTeam": "天圆数据管理研发团队",
-        "welcome": "欢迎访问云尚智慧物业管理系统",
+        "systemName": "HC智慧物业管理系统",
+        "systemSimpleName": "HC",
+        "subSystemName": "HC智慧物业管理系统",
+        "companyTeam": "java110研发团队",
+        "welcome": "欢迎访问HC智慧物业管理系统",
         "signOut": "退出",
         "signIn":"登录",
         "register":"注册",
         "moreCommunity":"更多小区",
         "moreMsg":"查看全部消息",
-        "title":"云尚智慧物业管理系统",
+        "title":"HC智慧物业管理系统",
         "noAccount":"还没有账号？ ",
         "areyouhasaccount":"已经有账户了？" 
     }
