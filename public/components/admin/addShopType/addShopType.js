42ee9a01b6aa4f9c108e4223365d2bb3df9f0704
java110 <928255095@qq.com>
2022-01-20 17:59:41 +0800

    优化代码

--- public/components/admin/addShopType/addShopType.js
+++ public/components/admin/addShopType/addShopType.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 (function(vc) {
-=======
-(function (vc) {
->>>>>>> c6f99a4136d2a6450c08d47bb328ee07cf4bdb68
 
     vc.extends({
         propTypes: {
@@ -21,19 +17,11 @@
 
             }
         },
-<<<<<<< HEAD
         _initMethod: function() {
 
         },
         _initEvent: function() {
             vc.on('addShopType', 'openAddShopTypeModal', function() {
-=======
-        _initMethod: function () {
-
-        },
-        _initEvent: function () {
-            vc.on('addShopType', 'openAddShopTypeModal', function () {
->>>>>>> c6f99a4136d2a6450c08d47bb328ee07cf4bdb68
                 $('#addShopTypeModel').modal('show');
             });
         },
@@ -43,12 +31,7 @@
                     addShopTypeInfo: vc.component.addShopTypeInfo
                 }, {
 
-<<<<<<< HEAD
                     'addShopTypeInfo.typeName': [{
-=======
-                    'addShopTypeInfo.typeName': [
-                        {
->>>>>>> c6f99a4136d2a6450c08d47bb328ee07cf4bdb68
                             limit: "required",
                             param: "",
                             errInfo: "店铺类型不能为空"
@@ -59,7 +42,6 @@
                             errInfo: "店铺类型太长"
                         },
                     ],
-<<<<<<< HEAD
                     'addShopTypeInfo.isShow': [{
                         limit: "required",
                         param: "",
@@ -71,22 +53,6 @@
                         errInfo: "是否默认不能为空"
                     }, ],
                     'addShopTypeInfo.seq': [{
-=======
-                    'addShopTypeInfo.isShow': [
-                        {
-                            limit: "required",
-                            param: "",
-                            errInfo: "是否展示不能为空"
-                        },],
-                    'addShopTypeInfo.isDefault': [
-                        {
-                            limit: "required",
-                            param: "",
-                            errInfo: "是否默认不能为空"
-                        },],
-                    'addShopTypeInfo.seq': [
-                        {
->>>>>>> c6f99a4136d2a6450c08d47bb328ee07cf4bdb68
                             limit: "required",
                             param: "",
                             errInfo: "显示序号不能为空"
@@ -95,18 +61,13 @@
                             limit: "num",
                             param: "",
                             errInfo: "显示序号不是有效数字"
-<<<<<<< HEAD
-=======
                         },
                     ],
-                    'addShopTypeInfo.remark': [
-                        {
-                            limit: "maxLength",
-                            param: "120",
-                            errInfo: "描述太长"
->>>>>>> c6f99a4136d2a6450c08d47bb328ee07cf4bdb68
-                        },
-                    ],
+                    'addShopTypeInfo.remark': [{
+                        limit: "maxLength",
+                        param: "120",
+                        errInfo: "描述太长"
+                    }, ],
                     'addShopTypeInfo.remark': [{
                         limit: "maxLength",
                         param: "120",
@@ -118,11 +79,7 @@
 
                 });
             },
-<<<<<<< HEAD
             saveShopTypeInfo: function() {
-=======
-            saveShopTypeInfo: function () {
->>>>>>> c6f99a4136d2a6450c08d47bb328ee07cf4bdb68
                 if (!vc.component.addShopTypeValidate()) {
                     vc.toast(vc.validate.errInfo);
 
@@ -138,18 +95,10 @@
 
                 vc.http.apiPost(
                     '/shopType/saveShopType',
-<<<<<<< HEAD
                     JSON.stringify(vc.component.addShopTypeInfo), {
                         emulateJSON: true
                     },
                     function(json, res) {
-=======
-                    JSON.stringify(vc.component.addShopTypeInfo),
-                    {
-                        emulateJSON: true
-                    },
-                    function (json, res) {
->>>>>>> c6f99a4136d2a6450c08d47bb328ee07cf4bdb68
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
                         let _json = JSON.parse(json);
                         if (_json.code == 0) {
@@ -163,22 +112,14 @@
                         vc.message(_json.msg);
 
                     },
-<<<<<<< HEAD
                     function(errInfo, error) {
-=======
-                    function (errInfo, error) {
->>>>>>> c6f99a4136d2a6450c08d47bb328ee07cf4bdb68
                         console.log('请求失败处理');
 
                         vc.message(errInfo);
 
                     });
             },
-<<<<<<< HEAD
             clearAddShopTypeInfo: function() {
-=======
-            clearAddShopTypeInfo: function () {
->>>>>>> c6f99a4136d2a6450c08d47bb328ee07cf4bdb68
                 vc.component.addShopTypeInfo = {
                     shopTypeId: '',
                     typeName: '',
