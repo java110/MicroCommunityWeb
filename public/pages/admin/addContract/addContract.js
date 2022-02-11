7d2a3a7ab2aa656e8f407dc89dfee15242638e9b
java110 <928255095@qq.com>
2022-02-11 17:53:13 +0800

    优化代码

--- public/pages/admin/addContract/addContract.js
+++ public/pages/admin/addContract/addContract.js
@@ -1,4 +1,4 @@
-(function (vc) {
+(function(vc) {
 
     vc.extends({
         data: {
@@ -40,7 +40,7 @@
                 contractPartyAs: []
             }
         },
-        _initMethod: function () {
+        _initMethod: function() {
             $that._loadAddContractType();
             $that._loadAddContractParkA();
 
@@ -54,28 +54,29 @@
                 $that.addContractInfo.contractName = '';
                 $that.addContractInfo.allNum = vc.getParam("objId");
                 $that._queryRoom();
+                $that._listContracts();
             }
 
-            vc.initDateTime('addStartTime', function (_value) {
+            vc.initDateTime('addStartTime', function(_value) {
                 $that.addContractInfo.startTime = _value;
             });
-            vc.initDateTime('addEndTime', function (_value) {
+            vc.initDateTime('addEndTime', function(_value) {
                 $that.addContractInfo.endTime = _value;
             });
 
-            vc.initDateTime('addSigningTime', function (_value) {
+            vc.initDateTime('addSigningTime', function(_value) {
                 $that.addContractInfo.signingTime = _value;
             });
 
             $that.addContractInfo.signingTime = vc.dateTimeFormat(new Date().getTime());
 
         },
-        _initEvent: function () {
-            vc.on('addContract', 'chooseRoom', function (param) {
+        _initEvent: function() {
+            vc.on('addContract', 'chooseRoom', function(param) {
                 $that.addContractInfo.rooms.push(param);
             })
 
-            vc.on('addContract', 'chooseOwner', function (param) {
+            vc.on('addContract', 'chooseOwner', function(param) {
                 $that.addContractInfo.partyB = param.name;
                 $that.addContractInfo.bContacts = param.name;
                 $that.addContractInfo.bLink = param.link;
@@ -87,8 +88,7 @@
                 return vc.validate.validate({
                     addContractInfo: vc.component.addContractInfo
                 }, {
-                    'addContractInfo.contractName': [
-                        {
+                    'addContractInfo.contractName': [{
                             limit: "required",
                             param: "",
                             errInfo: "合同名称不能为空"
@@ -99,8 +99,7 @@
                             errInfo: "合同名称不能超过64位"
                         },
                     ],
-                    'addContractInfo.contractCode': [
-                        {
+                    'addContractInfo.contractCode': [{
                             limit: "required",
                             param: "",
                             errInfo: "合同编号不能为空"
@@ -111,8 +110,7 @@
                             errInfo: "合同编号错误"
                         },
                     ],
-                    'addContractInfo.contractType': [
-                        {
+                    'addContractInfo.contractType': [{
                             limit: "required",
                             param: "",
                             errInfo: "合同类型不能为空"
@@ -123,8 +121,7 @@
                             errInfo: "合同类型格式错误"
                         },
                     ],
-                    'addContractInfo.partyA': [
-                        {
+                    'addContractInfo.partyA': [{
                             limit: "required",
                             param: "",
                             errInfo: "甲方不能为空"
@@ -135,8 +132,7 @@
                             errInfo: "甲方名称太长"
                         },
                     ],
-                    'addContractInfo.partyB': [
-                        {
+                    'addContractInfo.partyB': [{
                             limit: "required",
                             param: "",
                             errInfo: "乙方不能为空"
@@ -147,8 +143,7 @@
                             errInfo: "乙方名称太长"
                         },
                     ],
-                    'addContractInfo.aContacts': [
-                        {
+                    'addContractInfo.aContacts': [{
                             limit: "required",
                             param: "",
                             errInfo: "甲方联系人不能为空"
@@ -159,8 +154,7 @@
                             errInfo: "甲方联系人长度超过64位"
                         },
                     ],
-                    'addContractInfo.bContacts': [
-                        {
+                    'addContractInfo.bContacts': [{
                             limit: "required",
                             param: "",
                             errInfo: "乙方联系人不能为空"
@@ -171,8 +165,7 @@
                             errInfo: "甲方联系人长度超过64位"
                         },
                     ],
-                    'addContractInfo.aLink': [
-                        {
+                    'addContractInfo.aLink': [{
                             limit: "required",
                             param: "",
                             errInfo: "甲方联系电话不能为空"
@@ -183,8 +176,7 @@
                             errInfo: "甲方联系电话错误"
                         },
                     ],
-                    'addContractInfo.bLink': [
-                        {
+                    'addContractInfo.bLink': [{
                             limit: "required",
                             param: "",
                             errInfo: "乙方联系电话不能为空"
@@ -195,8 +187,7 @@
                             errInfo: "乙方联系电话错误"
                         },
                     ],
-                    'addContractInfo.operator': [
-                        {
+                    'addContractInfo.operator': [{
                             limit: "required",
                             param: "",
                             errInfo: "经办人不能为空"
@@ -207,8 +198,7 @@
                             errInfo: "经办人超过64位"
                         },
                     ],
-                    'addContractInfo.operatorLink': [
-                        {
+                    'addContractInfo.operatorLink': [{
                             limit: "required",
                             param: "",
                             errInfo: "联系电话不能为空"
@@ -219,15 +209,12 @@
                             errInfo: "经办人联系电话格式错误"
                         },
                     ],
-                    'addContractInfo.amount': [
-                        {
-                            limit: "money",
-                            param: "",
-                            errInfo: "合同金额格式错误，如1.50"
-                        },
-                    ],
-                    'addContractInfo.startTime': [
-                        {
+                    'addContractInfo.amount': [{
+                        limit: "money",
+                        param: "",
+                        errInfo: "合同金额格式错误，如1.50"
+                    }, ],
+                    'addContractInfo.startTime': [{
                             limit: "required",
                             param: "",
                             errInfo: "开始时间不能为空"
@@ -238,8 +225,7 @@
                             errInfo: "合同开始时间格式错误"
                         },
                     ],
-                    'addContractInfo.endTime': [
-                        {
+                    'addContractInfo.endTime': [{
                             limit: "required",
                             param: "",
                             errInfo: "结束时间不能为空"
@@ -250,8 +236,7 @@
                             errInfo: "合同结束时间格式错误"
                         },
                     ],
-                    'addContractInfo.signingTime': [
-                        {
+                    'addContractInfo.signingTime': [{
                             limit: "required",
                             param: "",
                             errInfo: "签订时间不能为空"
@@ -264,26 +249,27 @@
                     ],
                 });
             },
-            _queryRoom: function () {
+            _queryRoom: function() {
                 let param = {
-                    params: {
-                        contractId: vc.getParam("contractId"),
-                        page: 1,
-                        row: 100
+                        params: {
+                            contractId: vc.getParam("contractId"),
+                            page: 1,
+                            row: 100
+                        }
                     }
-                }
-                //发送get请求
+                    //发送get请求
                 vc.http.apiGet('/contract/queryContractRoom',
                     param,
-                    function (json, res) {
+                    function(json, res) {
                         var _contractTFile = JSON.parse(json);
                         vc.component.addContractInfo.rooms = _contractTFile.data;
-                    }, function (errInfo, error) {
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                     }
                 );
             },
-            saveContractInfo: function () {
+            saveContractInfo: function() {
                 if (!vc.component.addContractValidate()) {
                     vc.toast(vc.validate.errInfo);
                     return;
@@ -292,11 +278,10 @@
 
                 vc.http.apiPost(
                     '/contract/saveContract',
-                    JSON.stringify(vc.component.addContractInfo),
-                    {
+                    JSON.stringify(vc.component.addContractInfo), {
                         emulateJSON: true
                     },
-                    function (json, res) {
+                    function(json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
                         let _json = JSON.parse(json);
                         if (_json.code == 0) {
@@ -308,14 +293,14 @@
                         vc.toast(_json.msg);
 
                     },
-                    function (errInfo, error) {
+                    function(errInfo, error) {
                         console.log('请求失败处理');
 
                         vc.toast(errInfo);
 
                     });
             },
-            clearAddContractInfo: function () {
+            clearAddContractInfo: function() {
                 let _contractTypes = $that.addContractInfo.contractTypes;
                 vc.component.addContractInfo = {
                     contractName: '',
@@ -354,43 +339,45 @@
                     objType: '1111'
                 };
             },
-            _loadAddContractType: function () {
+            _loadAddContractType: function() {
                 let param = {
-                    params: {
-                        page: 1,
-                        row: 100
+                        params: {
+                            page: 1,
+                            row: 100
+                        }
                     }
-                }
-                //发送get请求
+                    //发送get请求
                 vc.http.apiGet('/contract/queryContractType',
                     param,
-                    function (json, res) {
+                    function(json, res) {
                         var _contractTypeManageInfo = JSON.parse(json);
                         vc.component.addContractInfo.contractTypes = _contractTypeManageInfo.data;
-                    }, function (errInfo, error) {
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                     }
                 );
             },
-            _loadAddContractParkA: function () {
+            _loadAddContractParkA: function() {
                 let param = {
-                    params: {
-                        page: 1,
-                        row: 100
+                        params: {
+                            page: 1,
+                            row: 100
+                        }
                     }
-                }
-                //发送get请求
+                    //发送get请求
                 vc.http.apiGet('/contractPartya/queryContractPartya',
                     param,
-                    function (json, res) {
+                    function(json, res) {
                         var _contractTypeManageInfo = JSON.parse(json);
                         vc.component.addContractInfo.contractPartyAs = _contractTypeManageInfo.data;
-                    }, function (errInfo, error) {
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                     }
                 );
             },
-            _changeContractType: function () {
+            _changeContractType: function() {
                 let param = {
                     params: {
                         page: 1,
@@ -401,7 +388,7 @@
                 $that.addContractInfo.contractTypeSpecs = [];
                 vc.http.apiGet('/contract/queryContractTypeSpec',
                     param,
-                    function (json, res) {
+                    function(json, res) {
                         let _contractTypeSpecManageInfo = JSON.parse(json);
                         _contractTypeSpecManageInfo.data.forEach(item => {
                             item.value = '';
@@ -411,18 +398,19 @@
                                 $that.addContractInfo.contractTypeSpecs.push(item);
                             }
                         });
-                    }, function (errInfo, error) {
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                     }
                 );
             },
-            _goBack: function () {
+            _goBack: function() {
                 vc.goBack();
             },
-            _selectRoom: function () {
+            _selectRoom: function() {
                 vc.emit('searchRoom', 'openSearchRoomModel', {})
             },
-            _openDelRoomModel: function (_room) {
+            _openDelRoomModel: function(_room) {
 
                 let _tmpRooms = [];
                 $that.addContractInfo.rooms.forEach(item => {
@@ -432,7 +420,7 @@
                 });
                 $that.addContractInfo.rooms = _tmpRooms;
             },
-            addFileStep: function () {
+            addFileStep: function() {
                 let _file = {
                     seq: $that.addContractInfo.contractFilePo.length,
                     fileSaveName: '',
@@ -441,21 +429,21 @@
                 $that.addContractInfo.contractFilePo.push(_file);
             },
 
-             
-            deleteStep: function (_step) {
+
+            deleteStep: function(_step) {
                 for (var i = 0; i < $that.addContractInfo.contractFilePo.length; i++) {
                     if ($that.addContractInfo.contractFilePo[i].seq == _step.seq) {
-                       
+
                         $that.addContractInfo.contractFilePo.splice(i, 1);
                     }
                 }
             },
-            getFile: function (e,index) {
+            getFile: function(e, index) {
                 vc.component.addContractInfo.tempfile = e.target.files[0];
                 $that.addContractInfo.contractFilePo[index].fileRealName = vc.component.addContractInfo.tempfile.name;
                 this._importData(index);
             },
-            _importData: function (index) {
+            _importData: function(index) {
                 // 导入数据
                 let _fileName = vc.component.addContractInfo.tempfile.name;
                 let _suffix = _fileName.substring(_fileName.lastIndexOf('.') + 1);
@@ -463,21 +451,20 @@
                     vc.toast('操作失败，请上传图片、PDF格式的文件');
                     return;
                 }
-           
+
                 var param = new FormData();
                 param.append("uploadFile", vc.component.addContractInfo.tempfile);
                 vc.http.upload(
                     'importRoomFee',
                     'uploadContactFile',
-                    param,
-                    {
+                    param, {
                         emulateJSON: true,
                         //添加请求头
                         headers: {
                             "Content-Type": "multipart/form-data"
                         }
                     },
-                    function (json, res) {
+                    function(json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
                         if (res.status == 200) {
                             $that.addContractInfo.contractFilePo[index].fileSaveName = json;
@@ -486,13 +473,13 @@
                         }
                         vc.toast(json, 10000);
                     },
-                    function (errInfo, error) {
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                         vc.toast(errInfo, 10000);
                     });
             },
-            checkFileType: function (fileType) {
-                const acceptTypes = ['png','pdf','jpg'];
+            checkFileType: function(fileType) {
+                const acceptTypes = ['png', 'pdf', 'jpg'];
                 for (var i = 0; i < acceptTypes.length; i++) {
                     if (fileType === acceptTypes[i]) {
                         return true;
@@ -500,19 +487,46 @@
                 }
                 return false;
             },
-            _changeContractPartyA:function(){
+            _changeContractPartyA: function() {
                 let _partyA = $that.addContractInfo.partyA;
-                $that.addContractInfo.contractPartyAs.forEach(item=>{
-                    if(_partyA == item.partyA){
+                $that.addContractInfo.contractPartyAs.forEach(item => {
+                    if (_partyA == item.partyA) {
                         $that.addContractInfo.aLink = item.aLink;
                         $that.addContractInfo.aContacts = item.aContacts;
 
                     }
                 })
             },
-            _searchOwner:function(){
-                vc.emit('searchOwner', 'openSearchOwnerModel',{});
-            }
+            _searchOwner: function() {
+                vc.emit('searchOwner', 'openSearchOwnerModel', {});
+            },
+            _listContracts: function() {
+                let param = {
+                    params: {
+                        page: 1,
+                        row: 1,
+                        contractId: vc.getParam('contractId')
+                    }
+                };
+
+                //发送get请求
+                vc.http.apiGet('/contract/queryContract',
+                    param,
+                    function(json, res) {
+                        let _expirationContractInfo = JSON.parse(json);
+                        vc.copyObject(_expirationContractInfo.data[0], vc.component.addContractInfo);
+                        $that.addContractInfo.contractId = '';
+                        $that.addContractInfo.contractCode = '';
+                        $that.addContractInfo.contractName = '';
+                        $that.addContractInfo.startTime = $that.addContractInfo.endTime;
+                        $that.addContractInfo.endTime = '';
+                        $that.addContractInfo.signingTime = '';
+                    },
+                    function(errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
+            },
         }
     });
 
--- public/pages/admin/contractChangeAuditOrders/contractChangeAuditOrders.js
+++ public/pages/admin/contractChangeAuditOrders/contractChangeAuditOrders.js
@@ -1,7 +1,7 @@
 /**
  审核订单
  **/
-(function (vc) {
+(function(vc) {
     var DEFAULT_PAGE = 1;
     var DEFAULT_ROWS = 10;
     vc.extends({
@@ -22,24 +22,24 @@
                 procure: false
             }
         },
-        _initMethod: function () {
+        _initMethod: function() {
             vc.component._listAuditOrders(DEFAULT_PAGE, DEFAULT_ROWS);
         },
-        _initEvent: function () {
+        _initEvent: function() {
 
-            vc.on('contractChangeAuditOrders', 'listAuditOrders', function (_param) {
+            vc.on('contractChangeAuditOrders', 'listAuditOrders', function(_param) {
                 vc.component._listAuditOrders(DEFAULT_PAGE, DEFAULT_ROWS);
             });
-            vc.on('pagination', 'page_event', function (_currentPage) {
+            vc.on('pagination', 'page_event', function(_currentPage) {
                 vc.component._listAuditOrders(_currentPage, DEFAULT_ROWS);
             });
 
-            vc.on('contractChangeAuditOrders', 'notifyAudit', function (_auditInfo) {
+            vc.on('contractChangeAuditOrders', 'notifyAudit', function(_auditInfo) {
                 vc.component._auditOrderInfo(_auditInfo);
             });
         },
         methods: {
-            _listAuditOrders: function (_page, _rows) {
+            _listAuditOrders: function(_page, _rows) {
 
                 vc.component.contractChangeAuditOrdersInfo.conditions.page = _page;
                 vc.component.contractChangeAuditOrdersInfo.conditions.row = _rows;
@@ -50,7 +50,7 @@
                 //发送get请求
                 vc.http.apiGet('/contract/queryContractChangeTask',
                     param,
-                    function (json, res) {
+                    function(json, res) {
                         var _contractChangeAuditOrdersInfo = JSON.parse(json);
                         vc.component.contractChangeAuditOrdersInfo.total = _contractChangeAuditOrdersInfo.total;
                         vc.component.contractChangeAuditOrdersInfo.records = _contractChangeAuditOrdersInfo.records;
@@ -60,42 +60,44 @@
                             dataCount: vc.component.contractChangeAuditOrdersInfo.total,
                             currentPage: _page
                         });
-                    }, function (errInfo, error) {
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                     }
                 );
             },
-            _openAuditOrderModel: function (_auditOrder) {
+            _openAuditOrderModel: function(_auditOrder) {
                 vc.component.contractChangeAuditOrdersInfo.orderInfo = _auditOrder;
                 vc.emit('audit', 'openAuditModal', {});
             },
-            _queryAuditOrdersMethod: function () {
+            _queryAuditOrdersMethod: function() {
                 vc.component._listAuditOrders(DEFAULT_PAGE, DEFAULT_ROWS);
             },
-            _openDetailPurchaseApplyModel: function (_purchaseApply) {
+            _openDetailPurchaseApplyModel: function(_purchaseApply) {
                 vc.jumpToPage("/admin.html#/pages/common/purchaseApplyDetail?applyOrderId=" + _purchaseApply.applyOrderId + "&resOrderType=" + _purchaseApply.resOrderType);
             },
             //提交审核信息
-            _auditOrderInfo: function (_auditInfo) {
+            _auditOrderInfo: function(_auditInfo) {
                 console.log("提交得参数：" + _auditInfo);
                 _auditInfo.taskId = vc.component.contractChangeAuditOrdersInfo.orderInfo.taskId;
                 _auditInfo.planId = vc.component.contractChangeAuditOrdersInfo.orderInfo.planId;
+                _auditInfo.contractId = vc.component.contractChangeAuditOrdersInfo.orderInfo.contractId;
                 //发送get请求
                 vc.http.apiPost('/contract/needAuditContractPlan',
-                    JSON.stringify(_auditInfo),
-                    {
+                    JSON.stringify(_auditInfo), {
                         emulateJSON: true
                     },
-                    function (json, res) {
+                    function(json, res) {
                         vc.toast("处理成功");
                         vc.component._listAuditOrders(DEFAULT_PAGE, DEFAULT_ROWS);
-                    }, function (errInfo, error) {
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                         vc.toast("处理失败：" + errInfo);
                     }
                 );
             },
-            _finishAuditOrder: function (_auditOrder) {
+            _finishAuditOrder: function(_auditOrder) {
                 let _auditInfo = {
                     taskId: _auditOrder.taskId,
                     planId: _auditOrder.planId,
@@ -104,20 +106,20 @@
                 };
                 //发送get请求
                 vc.http.apiPost('/contract/needAuditContractPlan',
-                    JSON.stringify(_auditInfo),
-                    {
+                    JSON.stringify(_auditInfo), {
                         emulateJSON: true
                     },
-                    function (json, res) {
+                    function(json, res) {
                         vc.toast("处理成功");
                         vc.component._listAuditOrders(DEFAULT_PAGE, DEFAULT_ROWS);
-                    }, function (errInfo, error) {
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                         vc.toast("处理失败：" + errInfo);
                     }
                 );
             },
-            _viewChangeDetail: function (_plan) {
+            _viewChangeDetail: function(_plan) {
                 vc.jumpToPage("/admin.html#/pages/admin/contractChangeDetails?planId=" + _plan.planId);
             }
 
