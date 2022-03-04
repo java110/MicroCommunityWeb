bed2b868ee6d4da23108b771c91379439f66e80a
java110 <928255095@qq.com>
2022-03-04 13:34:03 +0800

    开始开发结构化

---
+++ public/pages/property/roomStructure/roomStructure.js
@@ -0,0 +1,23 @@
+/**
+ 入驻小区
+ **/
+(function(vc) {
+    var DEFAULT_PAGE = 1;
+    var DEFAULT_ROWS = 10;
+    vc.extends({
+        data: {
+            roomStructureInfo: {
+
+            }
+        },
+        _initMethod: function() {
+
+        },
+        _initEvent: function() {
+
+        },
+        methods: {
+
+        }
+    });
+})(window.vc);
