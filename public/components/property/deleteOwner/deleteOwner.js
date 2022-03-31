9f3c5567926895300ec6d2d5dbccf7df41b7c151
java110 <928255095@qq.com>
2022-03-31 09:22:13 +0800

    优化 业主 删除为cmd 模式

--- public/components/property/deleteOwner/deleteOwner.js
+++ public/components/property/deleteOwner/deleteOwner.js
@@ -1,4 +1,4 @@
-(function (vc) {
+(function(vc) {
     vc.extends({
         propTypes: {
             notifyLoadDataComponentName: vc.propTypes.string
@@ -6,26 +6,24 @@
         data: {
             deleteOwnerInfo: {}
         },
-        _initEvent: function () {
-            vc.on('deleteOwner', 'openOwnerModel', function (_ownerInfo) {
+        _initEvent: function() {
+            vc.on('deleteOwner', 'openOwnerModel', function(_ownerInfo) {
                 vc.component.deleteOwnerInfo = _ownerInfo;
                 $('#deleteOwnerModel').modal('show');
             });
         },
         methods: {
-            closeDeleteOwnerModel: function () {
+            closeDeleteOwnerModel: function() {
                 $('#deleteOwnerModel').modal('hide');
             },
-            deleteOwner: function () {
+            deleteOwner: function() {
                 vc.component.deleteOwnerInfo.communityId = vc.getCurrentCommunity().communityId;
-                vc.http.post(
-                    'deleteOwner',
-                    'delete',
-                    JSON.stringify(vc.component.deleteOwnerInfo),
-                    {
+                vc.http.apiPost(
+                    '/owner.deleteOwner',
+                    JSON.stringify(vc.component.deleteOwnerInfo), {
                         emulateJSON: true
                     },
-                    function (json, res) {
+                    function(json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
                         if (res.status == 200) {
                             //关闭model
@@ -35,7 +33,7 @@
                         }
                         vc.component.deleteOwnernfo.errorInfo = json;
                     },
-                    function (errInfo, error) {
+                    function(errInfo, error) {
                         vc.toast(errInfo);
                         // vc.component.deleteOwnernfo.errorInfo = errInfo;
                     });
