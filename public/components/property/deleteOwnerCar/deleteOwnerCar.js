c5d1585bb82ad006b445d0134a51b05c163bc0ac
java110 <928255095@qq.com>
2022-04-12 16:09:07 +0800

    优化删除

--- public/components/property/deleteOwnerCar/deleteOwnerCar.js
+++ public/components/property/deleteOwnerCar/deleteOwnerCar.js
@@ -1,43 +1,43 @@
-(function(vc){
+(function(vc) {
     vc.extends({
         propTypes: {
-                notifyLoadDataComponentName:vc.propTypes.string
+            notifyLoadDataComponentName: vc.propTypes.string
         },
-        data:{
-            deleteOwnerCarInfo:{}
+        data: {
+            deleteOwnerCarInfo: {}
         },
-        _initEvent:function(){
-             vc.on('deleteOwnerCar','openOwnerCarModel',function(_ownerInfo){
-                    vc.component.deleteOwnerCarInfo = _ownerInfo;
-                    $('#deleteOwnerCarModel').modal('show');
-                });
+        _initEvent: function() {
+            vc.on('deleteOwnerCar', 'openOwnerCarModel', function(_ownerInfo) {
+                vc.component.deleteOwnerCarInfo = _ownerInfo;
+                $('#deleteOwnerCarModel').modal('show');
+            });
         },
-        methods:{
-            closedeleteOwnerCarModel:function(){
+        methods: {
+            closedeleteOwnerCarModel: function() {
                 $('#deleteOwnerCarModel').modal('hide');
             },
-            deleteOwnerCar:function(){
+            deleteOwnerCar: function() {
                 vc.component.deleteOwnerCarInfo.communityId = vc.getCurrentCommunity().communityId;
                 vc.http.apiPost(
                     'owner.deleteOwnerCars',
-                    JSON.stringify(vc.component.deleteOwnerCarInfo),
-                    {
-                        emulateJSON:true
-                     },
-                     function(json,res){
+                    JSON.stringify(vc.component.deleteOwnerCarInfo), {
+                        emulateJSON: true
+                    },
+                    function(json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
-                        if(res.status == 200){
+                        let _json = JSON.parse(json)
+                        if (_json.code == 0) {
                             //关闭model
                             $('#deleteOwnerCarModel').modal('hide');
-                            vc.emit($props.notifyLoadDataComponentName,'listOwnerCarData',{});
-                            return ;
+                            vc.emit($props.notifyLoadDataComponentName, 'listOwnerCarData', {});
+                            return;
                         }
-                        vc.component.deleteOwnerCarnfo.errorInfo = json;
-                     },
-                     function(errInfo,error){
-                         vc.toast(errInfo);
-                       // vc.component.deleteOwnerCarnfo.errorInfo = errInfo;
-                     });
+                        vc.toast(_json.msg);
+                    },
+                    function(errInfo, error) {
+                        vc.toast(errInfo);
+                        // vc.component.deleteOwnerCarnfo.errorInfo = errInfo;
+                    });
             }
         }
     });
