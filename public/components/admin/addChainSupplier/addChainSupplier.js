929a204ca53bd38c54a33c6c4a2be2b157e51eeb
shane <88386378@qq.com>
2022-06-18 08:30:07 +0800

    供应链供应商

--- public/components/admin/addChainSupplier/addChainSupplier.js
+++ public/components/admin/addChainSupplier/addChainSupplier.js
@@ -1,179 +1,179 @@
-(function(vc){
+(function (vc) {
 
     vc.extends({
         propTypes: {
-               callBackListener:vc.propTypes.string, //父组件名称
-               callBackFunction:vc.propTypes.string //父组件监听方法
+            callBackListener: vc.propTypes.string, //父组件名称
+            callBackFunction: vc.propTypes.string //父组件监听方法
         },
-        data:{
-            addChainSupplierInfo:{
-                csId:'',
-                name:'',
-appId:'',
-appSecure:'',
-mchId:'',
-mchKey:'',
-url:'',
-remark:'',
+        data: {
+            addChainSupplierInfo: {
+                csId: '',
+                name: '',
+                appId: '',
+                appSecure: '',
+                mchId: '',
+                mchKey: '',
+                url: '',
+                remark: '',
+                beanName:'',
+                statusCd: '0'
 
             }
         },
-         _initMethod:function(){
+        _initMethod: function () {
 
-         },
-         _initEvent:function(){
-            vc.on('addChainSupplier','openAddChainSupplierModal',function(){
+        },
+        _initEvent: function () {
+            vc.on('addChainSupplier', 'openAddChainSupplierModal', function () {
                 $('#addChainSupplierModel').modal('show');
             });
         },
-        methods:{
-            addChainSupplierValidate(){
+        methods: {
+            addChainSupplierValidate() {
                 return vc.validate.validate({
-                    addChainSupplierInfo:vc.component.addChainSupplierInfo
-                },{
-                    'addChainSupplierInfo.name':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"供应商名称不能为空"
+                    addChainSupplierInfo: vc.component.addChainSupplierInfo
+                }, {
+                    'addChainSupplierInfo.name': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "供应商名称不能为空"
                         },
- {
-                            limit:"maxLength",
-                            param:"30",
-                            errInfo:"供应商名称不能超过30"
-                        },
-                    ],
-'addChainSupplierInfo.appId':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"接口应用ID不能为空"
-                        },
- {
-                            limit:"maxLength",
-                            param:"32",
-                            errInfo:"接口应用ID不能为空"
+                        {
+                            limit: "maxLength",
+                            param: "30",
+                            errInfo: "供应商名称不能超过30"
                         },
                     ],
-'addChainSupplierInfo.appSecure':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"接口秘钥不能为空"
+                    'addChainSupplierInfo.appId': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "接口应用ID不能为空"
                         },
- {
-                            limit:"maxLength",
-                            param:"128",
-                            errInfo:"接口秘钥不能为空"
-                        },
-                    ],
-'addChainSupplierInfo.mchId':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"商户ID不能为空"
-                        },
- {
-                            limit:"maxLength",
-                            param:"32",
-                            errInfo:"商户ID不能为空"
+                        {
+                            limit: "maxLength",
+                            param: "32",
+                            errInfo: "接口应用ID不能为空"
                         },
                     ],
-'addChainSupplierInfo.mchKey':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"秘钥不能为空"
+                    'addChainSupplierInfo.appSecure': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "接口秘钥不能为空"
                         },
- {
-                            limit:"maxLength",
-                            param:"128",
-                            errInfo:"秘钥不能为空"
-                        },
-                    ],
-'addChainSupplierInfo.url':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"接口地址不能为空"
-                        },
- {
-                            limit:"maxLength",
-                            param:"128",
-                            errInfo:"接口地址不能为空"
+                        {
+                            limit: "maxLength",
+                            param: "128",
+                            errInfo: "接口秘钥不能为空"
                         },
                     ],
-'addChainSupplierInfo.remark':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"备注不能为空"
+                    'addChainSupplierInfo.mchId': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "商户ID不能为空"
                         },
- {
-                            limit:"maxLength",
-                            param:"200",
-                            errInfo:"描述不能为空"
+                        {
+                            limit: "maxLength",
+                            param: "32",
+                            errInfo: "商户ID不能为空"
                         },
                     ],
-
-
-
+                    'addChainSupplierInfo.mchKey': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "秘钥不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "128",
+                            errInfo: "秘钥不能为空"
+                        },
+                    ],
+                    'addChainSupplierInfo.url': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "接口地址不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "128",
+                            errInfo: "接口地址不能为空"
+                        },
+                    ],
+                    'addChainSupplierInfo.beanName': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "适配器名称不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "50",
+                            errInfo: "适配器名称不能为空"
+                        },
+                    ],
 
                 });
             },
-            saveChainSupplierInfo:function(){
-                if(!vc.component.addChainSupplierValidate()){
+            saveChainSupplierInfo: function () {
+                if (!vc.component.addChainSupplierValidate()) {
                     vc.toast(vc.validate.errInfo);
 
-                    return ;
+                    return;
                 }
 
                 vc.component.addChainSupplierInfo.communityId = vc.getCurrentCommunity().communityId;
                 //不提交数据将数据 回调给侦听处理
-                if(vc.notNull($props.callBackListener)){
-                    vc.emit($props.callBackListener,$props.callBackFunction,vc.component.addChainSupplierInfo);
+                if (vc.notNull($props.callBackListener)) {
+                    vc.emit($props.callBackListener, $props.callBackFunction, vc.component.addChainSupplierInfo);
                     $('#addChainSupplierModel').modal('hide');
-                    return ;
+                    return;
                 }
 
                 vc.http.apiPost(
                     'chainSupplier.saveChainSupplier',
                     JSON.stringify(vc.component.addChainSupplierInfo),
                     {
-                        emulateJSON:true
-                     },
-                     function(json,res){
+                        emulateJSON: true
+                    },
+                    function (json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
                         let _json = JSON.parse(json);
                         if (_json.code == 0) {
                             //关闭model
                             $('#addChainSupplierModel').modal('hide');
                             vc.component.clearAddChainSupplierInfo();
-                            vc.emit('chainSupplierManage','listChainSupplier',{});
+                            vc.emit('chainSupplierManage', 'listChainSupplier', {});
 
-                            return ;
+                            return;
                         }
                         vc.message(_json.msg);
 
-                     },
-                     function(errInfo,error){
+                    },
+                    function (errInfo, error) {
                         console.log('请求失败处理');
 
                         vc.message(errInfo);
 
-                     });
+                    });
             },
-            clearAddChainSupplierInfo:function(){
+            clearAddChainSupplierInfo: function () {
                 vc.component.addChainSupplierInfo = {
-                                            name:'',
-appId:'',
-appSecure:'',
-mchId:'',
-mchKey:'',
-url:'',
-remark:'',
+                    name: '',
+                    appId: '',
+                    appSecure: '',
+                    mchId: '',
+                    mchKey: '',
+                    url: '',
+                    beanName:'',
+                    remark: '',
 
-                                        };
+                };
             }
         }
     });
