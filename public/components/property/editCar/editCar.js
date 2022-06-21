1d5484ddc7ecc4c5e171f12054d793afc026d2ee
java110 <928255095@qq.com>
2022-06-21 14:11:08 +0800

    优化代码

--- public/components/property/editCar/editCar.js
+++ public/components/property/editCar/editCar.js
@@ -132,7 +132,9 @@
                     },
                     function(json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
-                        if (res.status == 200) {
+                        let _json = JSON.parse(json)
+
+                        if (_json.code != 0) {
                             //关闭model
                             $('#editCarModal').modal('hide');
                             vc.emit('listOwnerCar', 'listOwnerCarData', {});
@@ -143,7 +145,7 @@
                             }
                             return;
                         }
-                        vc.toast(json);
+                        vc.toast(_json.msg);
                     },
                     function(errInfo, error) {
                         console.log('请求失败处理');
