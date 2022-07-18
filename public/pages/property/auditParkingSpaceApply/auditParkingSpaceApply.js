98ef7635e91806b93a49ef417f9d1f2e52b1b560
java110 <928255095@qq.com>
2022-07-18 19:32:41 +0800

    优化代码

--- public/pages/property/auditParkingSpaceApply/auditParkingSpaceApply.js
+++ public/pages/property/auditParkingSpaceApply/auditParkingSpaceApply.js
@@ -1,13 +1,11 @@
-(function (vc) {
+(function(vc) {
 
     vc.extends({
-     
+
         data: {
-            addParkingSpaceApplyInfo: {
-                paName: '',
-                paId: '',
+            auditParkingSpaceApplyInfo: {
                 psId: '',
-                psName:'',
+                psName: '',
                 applyId: '',
                 carNum: '',
                 carBrand: '',
@@ -22,14 +20,14 @@
                 remark: '',
                 configId: '',
                 remark2: '',
-                feeConfigs:[]
+                feeConfigs: []
 
             }
         },
-        _initMethod: function () {
-            vc.component.addParkingSpaceApplyInfo.applyId = vc.getParam('applyId');
-            vc.component._listParkingSpaceApply();
-            vc.component._listFeeConfigs();
+        _initMethod: function() {
+            $that.auditParkingSpaceApplyInfo.applyId = vc.getParam('applyId');
+            $that._listParkingSpaceApply();
+            $that._listFeeConfigs();
 
             $('.editStartTime').datetimepicker({
                 language: 'zh-CN',
@@ -41,10 +39,10 @@
                 todayBtn: true
             });
             $('.editStartTime').datetimepicker()
-                .on('changeDate', function (ev) {
+                .on('changeDate', function(ev) {
                     var value = $(".editStartTime").val();
-                    vc.component.addParkingSpaceApplyInfo.startTime = value;
-            });
+                    $that.auditParkingSpaceApplyInfo.startTime = value;
+                });
             $('.editEndTime').datetimepicker({
                 language: 'zh-CN',
                 fontAwesome: 'fa',
@@ -55,124 +53,120 @@
                 todayBtn: true
             });
             $('.editEndTime').datetimepicker()
-                .on('changeDate', function (ev) {
+                .on('changeDate', function(ev) {
                     var value = $(".editEndTime").val();
-                    vc.component.addParkingSpaceApplyInfo.endTime = value;
-            });
+                    $that.auditParkingSpaceApplyInfo.endTime = value;
+                });
         },
-        _initEvent: function () {
-            vc.on('addParkingSpaceApply', 'chooseParkingArea', function (_parkingArea) {
-                vc.component.addParkingSpaceApplyInfo.paId = _parkingArea.paId;
-                vc.component.addParkingSpaceApplyInfo.paName = _parkingArea.num;
+        _initEvent: function() {
+            vc.on('auditParkingSpaceApply', 'chooseParkingSpace', function(_parkingArea) {
+                $that.auditParkingSpaceApplyInfo.psId = _parkingArea.psId;
+                $that.auditParkingSpaceApplyInfo.psName = _parkingArea.num;
             });
-      
+
         },
         methods: {
-
-              //查询方法
-              _listFeeConfigs: function (){
+            //查询方法
+            _listFeeConfigs: function() {
                 var param = {
                     params: {
                         page: 1,
                         row: 50,
-                        feeTypeCd: "888800010008",//默认停车费
-                        communityId : vc.getCurrentCommunity().communityId
+                        feeTypeCd: "888800010008", //默认停车费
+                        communityId: vc.getCurrentCommunity().communityId
                     }
                 };
                 //发送get请求
                 vc.http.get('feeConfigManage', 'list', param,
-                    function (json, res) {
-                        var _feeConfigManageInfo = JSON.parse(json);
-                        vc.component.addParkingSpaceApplyInfo.feeConfigs = _feeConfigManageInfo.feeConfigs;
+                    function(json, res) {
+                        let _feeConfigManageInfo = JSON.parse(json);
+                        $that.auditParkingSpaceApplyInfo.feeConfigs = _feeConfigManageInfo.feeConfigs;
                     },
-                    function (errInfo, error) {
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                     });
             },
-            addParkingSpaceApplyValidate() {
+            auditParkingSpaceApplyValidate() {
                 return vc.validate.validate({
-                    addParkingSpaceApplyInfo: vc.component.addParkingSpaceApplyInfo
+                    auditParkingSpaceApplyInfo: $that.auditParkingSpaceApplyInfo
                 }, {
-                    'addParkingSpaceApplyInfo.psId': [
-                        {
-                            limit: "required",
-                            param: "",
-                            errInfo: "车位不能为空"
-                        }
-                    ],
-                    'addParkingSpaceApplyInfo.configId': [
-                        {
-                            limit: "required",
-                            param: "",
-                            errInfo: "费用项不能为空"
-                        }
-                    ]
+                    'auditParkingSpaceApplyInfo.psId': [{
+                        limit: "required",
+                        param: "",
+                        errInfo: "车位不能为空"
+                    }],
+                    'auditParkingSpaceApplyInfo.configId': [{
+                        limit: "required",
+                        param: "",
+                        errInfo: "费用项不能为空"
+                    }]
                 });
             },
-            _listParkingSpaceApply:function(){
-                var param = {
+            _listParkingSpaceApply: function() {
+                let param = {
                     params: {
                         page: 1,
                         row: 1,
-                        applyId: vc.component.addParkingSpaceApplyInfo.applyId
+                        applyId: $that.auditParkingSpaceApplyInfo.applyId
                     }
-               };
+                };
 
-               //发送get请求
-               vc.http.apiGet('parkingSpaceApply.listParkingSpaceApply',
-                             param,
-                             function(json,res){
-                                var _parkingSpaceApplyManageInfo=JSON.parse(json);
-                                var _apply = _parkingSpaceApplyManageInfo.data[0];
-                                vc.copyObject(_apply, vc.component.addParkingSpaceApplyInfo);
-                             },function(errInfo,error){
-                                console.log('请求失败处理');
-                             }
-                           );
+                //发送get请求
+                vc.http.apiGet('parkingSpaceApply.listParkingSpaceApply',
+                    param,
+                    function(json, res) {
+                        let _parkingSpaceApplyManageInfo = JSON.parse(json);
+                        let _apply = _parkingSpaceApplyManageInfo.data[0];
+                        vc.copyObject(_apply, $that.auditParkingSpaceApplyInfo);
+                        $that.auditParkingSpaceApplyInfo.state = '';
+                    },
+                    function(errInfo, error) {
+                        console.log('请求失败处理');
+                    }
+                );
             },
-            
-            auditParkingSpaceApplyInfo: function () {
-                if (vc.component.addParkingSpaceApplyInfo.state == "") {
+
+            _doAuditParkingSpaceApply: function() {
+                if (!$that.auditParkingSpaceApplyInfo.state) {
                     vc.toast("请选择审核结果");
                     return;
                 }
-                if (vc.component.addParkingSpaceApplyInfo.state == 2002 && !vc.component.addParkingSpaceApplyValidate()) {
+                if ($that.auditParkingSpaceApplyInfo.state == '2002' && !$that.auditParkingSpaceApplyValidate()) {
                     vc.toast(vc.validate.errInfo);
                     return;
                 }
-                if (vc.component.addParkingSpaceApplyInfo.state == 4004 && vc.component.addParkingSpaceApplyInfo.remark2 == "") {
+                if ($that.auditParkingSpaceApplyInfo.state == '4004' && !$that.auditParkingSpaceApplyInfo.remark2) {
                     vc.toast("审核结果不通过时，必须填写意见");
                     return;
                 }
-                vc.component.addParkingSpaceApplyInfo.remark = vc.component.addParkingSpaceApplyInfo.remark+"审核意见："+vc.component.addParkingSpaceApplyInfo.remark2;
+                $that.auditParkingSpaceApplyInfo.remark = $that.auditParkingSpaceApplyInfo.remark + "审核意见：" + $that.auditParkingSpaceApplyInfo.remark2;
                 vc.http.apiPost(
-                    'parkingSpaceApply.updateParkingSpaceApply',
-                    JSON.stringify(vc.component.addParkingSpaceApplyInfo),
-                    {
-                        emulateJSON:true
-                     },
-                     function(json,res){
+                    '/parkingSpaceApply.updateParkingSpaceApply',
+                    JSON.stringify($that.auditParkingSpaceApplyInfo), {
+                        emulateJSON: true
+                    },
+                    function(json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
                         let _json = JSON.parse(json);
                         if (_json.code == 0) {
                             //关闭model
                             vc.toast('保存成功');
-                            vc.component.clearAddParkingSpaceApplyInfo();
+                            $that.clearAddParkingSpaceApplyInfo();
                             $that._goBack();
-                            return ;
+                            return;
                         }
                         vc.message(_json.msg);
-                     },
-                     function(errInfo,error){
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
 
                         vc.message(errInfo);
-                     });
+                    });
             },
-            clearAddParkingSpaceApplyInfo: function () {
-                vc.component.addParkingSpaceApplyInfo = {
-                    paId: '',
+            clearAddParkingSpaceApplyInfo: function() {
+                $that.auditParkingSpaceApplyInfo = {
                     psId: '',
+                    psName: '',
                     carNum: '',
                     carBrand: '',
                     carType: '',
@@ -187,14 +181,14 @@
 
                 };
             },
-            _goBack: function () {
+            _goBack: function() {
                 vc.goBack();
             },
-            _openChooseParkingArea: function () {
+            _openChooseParkingArea: function() {
                 vc.emit('chooseParkingArea', 'openChooseParkingAreaModel', {});
             },
-            _openChooseParkingSpace: function () {
-                vc.emit('chooseParkingSpace', 'openChooseParkingSpaceModel', $that.addParkingSpaceApplyInfo);
+            _openChooseParkingSpace: function() {
+                vc.emit('searchParkingSpace', 'openSearchParkingSpaceModel', {});
             },
         }
     });
