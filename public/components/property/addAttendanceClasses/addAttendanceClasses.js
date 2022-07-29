1c056d8089ca6767c53ebd0bbc545b28c304444f
java110 <928255095@qq.com>
2022-07-29 12:41:42 +0800

    优化考勤

--- public/components/property/addAttendanceClasses/addAttendanceClasses.js
+++ public/components/property/addAttendanceClasses/addAttendanceClasses.js
@@ -1,4 +1,4 @@
-(function (vc) {
+(function(vc) {
     vc.extends({
         propTypes: {
             callBackListener: vc.propTypes.string, //父组件名称
@@ -22,16 +22,16 @@
                 attrs: []
             }
         },
-        _initMethod: function () {
-            vc.getDict('attendance_classes', "clock_type", function (_data) {
+        _initMethod: function() {
+            vc.getDict('attendance_classes', "clock_type", function(_data) {
                 vc.component.addAttendanceClassesInfo.clockTypes = _data;
             });
         },
-        _initEvent: function () {
-            vc.on('addAttendanceClasses', 'openAddAttendanceClassesModal', function () {
+        _initEvent: function() {
+            vc.on('addAttendanceClasses', 'openAddAttendanceClassesModal', function() {
                 $('#addAttendanceClassesModel').modal('show');
             });
-            vc.on('addAttendanceClasses', 'staffSelect2', 'setStaff', function (_param) {
+            vc.on('addAttendanceClasses', 'staffSelect2', 'setStaff', function(_param) {
                 $that.addAttendanceClassesInfo.classesObjType = '1003';
                 $that.addAttendanceClassesInfo.classesObjId = _param.orgId;
                 $that.addAttendanceClassesInfo.classesObjName = _param.orgName;
@@ -42,8 +42,7 @@
                 return vc.validate.validate({
                     addAttendanceClassesInfo: vc.component.addAttendanceClassesInfo
                 }, {
-                    'addAttendanceClassesInfo.classesName': [
-                        {
+                    'addAttendanceClassesInfo.classesName': [{
                             limit: "required",
                             param: "",
                             errInfo: "班次名称不能为空"
@@ -54,8 +53,7 @@
                             errInfo: "班次名称格式错误"
                         },
                     ],
-                    'addAttendanceClassesInfo.timeOffset': [
-                        {
+                    'addAttendanceClassesInfo.timeOffset': [{
                             limit: "required",
                             param: "",
                             errInfo: "打卡范围不能为空"
@@ -66,8 +64,7 @@
                             errInfo: "打卡范围格式错误"
                         },
                     ],
-                    'addAttendanceClassesInfo.clockCount': [
-                        {
+                    'addAttendanceClassesInfo.clockCount': [{
                             limit: "required",
                             param: "",
                             errInfo: "打卡次数不能为空"
@@ -78,8 +75,7 @@
                             errInfo: "打卡次数错误"
                         },
                     ],
-                    'addAttendanceClassesInfo.clockType': [
-                        {
+                    'addAttendanceClassesInfo.clockType': [{
                             limit: "required",
                             param: "",
                             errInfo: "打卡类型不能为空"
@@ -90,8 +86,7 @@
                             errInfo: "打卡类型错误"
                         },
                     ],
-                    'addAttendanceClassesInfo.clockTypeValue': [
-                        {
+                    'addAttendanceClassesInfo.clockTypeValue': [{
                             limit: "required",
                             param: "",
                             errInfo: "打卡规则不能为空"
@@ -102,8 +97,7 @@
                             errInfo: "打卡规则格式错误"
                         },
                     ],
-                    'addAttendanceClassesInfo.leaveOffset': [
-                        {
+                    'addAttendanceClassesInfo.leaveOffset': [{
                             limit: "required",
                             param: "",
                             errInfo: "迟到范围不能为空"
@@ -114,8 +108,7 @@
                             errInfo: "迟到范围错误"
                         },
                     ],
-                    'addAttendanceClassesInfo.lateOffset': [
-                        {
+                    'addAttendanceClassesInfo.lateOffset': [{
                             limit: "required",
                             param: "",
                             errInfo: "早退范围不能为空"
@@ -126,8 +119,7 @@
                             errInfo: "早退范围错误"
                         },
                     ],
-                    'addAttendanceClassesInfo.classesObjType': [
-                        {
+                    'addAttendanceClassesInfo.classesObjType': [{
                             limit: "required",
                             param: "",
                             errInfo: "班次对象类型不能为空"
@@ -138,8 +130,7 @@
                             errInfo: "班次对象类型错误"
                         },
                     ],
-                    'addAttendanceClassesInfo.classesObjId': [
-                        {
+                    'addAttendanceClassesInfo.classesObjId': [{
                             limit: "required",
                             param: "",
                             errInfo: "班次对象不能为空"
@@ -152,7 +143,7 @@
                     ]
                 });
             },
-            saveAttendanceClassesInfo: function () {
+            saveAttendanceClassesInfo: function() {
                 let _clockType = $that.addAttendanceClassesInfo.clockType;
                 if (_clockType == '1001') {
                     $that.addAttendanceClassesInfo.clockTypeValue = '*';
@@ -173,7 +164,7 @@
                     return;
                 }
                 var flag = "0";
-                $that.addAttendanceClassesInfo.attrs.forEach(function (item) {
+                $that.addAttendanceClassesInfo.attrs.forEach(function(item) {
                     if (item.value == null || item.value == '' || item.value == 'undefined') {
                         vc.toast("上下班时间不能为空");
                         flag = "1";
@@ -190,12 +181,11 @@
                     return;
                 }
                 vc.http.apiPost(
-                    'attendanceClasses.saveAttendanceClasses',
-                    JSON.stringify(vc.component.addAttendanceClassesInfo),
-                    {
+                    '/attendanceClasses.saveAttendanceClasses',
+                    JSON.stringify(vc.component.addAttendanceClassesInfo), {
                         emulateJSON: true
                     },
-                    function (json, res) {
+                    function(json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
                         let _json = JSON.parse(json);
                         if (_json.code == 0) {
@@ -207,12 +197,12 @@
                             return;
                         }
                     },
-                    function (errInfo, error) {
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                         vc.toast(errInfo);
                     });
             },
-            clearAddAttendanceClassesInfo: function () {
+            clearAddAttendanceClassesInfo: function() {
                 vc.component.addAttendanceClassesInfo = {
                     classesName: '',
                     timeOffset: '',
@@ -228,72 +218,60 @@
                     attrs: []
                 };
             },
-            _addAttendanceChangeClockCount: function () {
+            _addAttendanceChangeClockCount: function() {
                 let _clockCount = $that.addAttendanceClassesInfo.clockCount;
                 let _attrs = [];
                 if (_clockCount > 1) {
-                    _attrs.push(
-                        {
-                            specCd: '10000',
-                            value: '',
-                            name: '上午上班',
-                            seq: 1
-                        }
-                    );
-                    _attrs.push(
-                        {
-                            specCd: '20000',
-                            value: '',
-                            name: '下午下班',
-                            seq: 4
-                        }
-                    );
+                    _attrs.push({
+                        specCd: '10000',
+                        value: '',
+                        name: '上午上班',
+                        seq: 1
+                    });
+                    _attrs.push({
+                        specCd: '20000',
+                        value: '',
+                        name: '下午下班',
+                        seq: 4
+                    });
                 }
                 if (_clockCount > 3) {
-                    _attrs.push(
-                        {
-                            specCd: '11000',
-                            value: '',
-                            name: '中午下班',
-                            seq: 2
-                        }
-                    );
-                    _attrs.push(
-                        {
-                            specCd: '21000',
-                            value: '',
-                            name: '中午上班',
-                            seq: 3
-                        }
-                    );
+                    _attrs.push({
+                        specCd: '11000',
+                        value: '',
+                        name: '中午下班',
+                        seq: 2
+                    });
+                    _attrs.push({
+                        specCd: '21000',
+                        value: '',
+                        name: '中午上班',
+                        seq: 3
+                    });
                 }
                 if (_clockCount > 5) {
-                    _attrs.push(
-                        {
-                            specCd: '12000',
-                            value: '',
-                            name: '晚上上班',
-                            seq: 5
-                        }
-                    );
-                    _attrs.push(
-                        {
-                            specCd: '22000',
-                            value: '',
-                            name: '晚上下班',
-                            seq: 6
-                        }
-                    );
+                    _attrs.push({
+                        specCd: '12000',
+                        value: '',
+                        name: '晚上上班',
+                        seq: 5
+                    });
+                    _attrs.push({
+                        specCd: '22000',
+                        value: '',
+                        name: '晚上下班',
+                        seq: 6
+                    });
                 }
-                let _newAttrs = _attrs.sort(function (a, b) {
+                let _newAttrs = _attrs.sort(function(a, b) {
                     return a.seq - b.seq;
                 });
                 $that.addAttendanceClassesInfo.attrs = _newAttrs;
-                $that.$nextTick(function () {
+                $that.$nextTick(function() {
                     //方法
                     $that.addAttendanceClassesInfo.attrs.forEach(item => {
                         //初始化日期组件
-                        vc.initHourMinute(item.specCd, function (_value) {
+                        vc.initHourMinute(item.specCd, function(_value) {
                             item.value = _value;
                         });
                     });
