4ffc954f462cc5c7fb21b5ec3241ec98d0bad359
java110 <928255095@qq.com>
2022-08-17 12:15:42 +0800

    优化代码

--- public/pages/property/machineRecordManage/machineRecordManage.js
+++ public/pages/property/machineRecordManage/machineRecordManage.js
@@ -1,7 +1,7 @@
 /**
  入驻小区
  **/
-(function (vc) {
+(function(vc) {
     var DEFAULT_PAGE = 1;
     var DEFAULT_ROWS = 10;
     vc.extends({
@@ -23,20 +23,20 @@
                 }
             }
         },
-        _initMethod: function () {
+        _initMethod: function() {
             vc.component._listMachineRecords(DEFAULT_PAGE, DEFAULT_ROWS);
         },
-        _initEvent: function () {
+        _initEvent: function() {
 
-            vc.on('machineRecordManage', 'listMachineRecord', function (_param) {
+            vc.on('machineRecordManage', 'listMachineRecord', function(_param) {
                 vc.component._listMachineRecords(DEFAULT_PAGE, DEFAULT_ROWS);
             });
-            vc.on('pagination', 'page_event', function (_currentPage) {
+            vc.on('pagination', 'page_event', function(_currentPage) {
                 vc.component._listMachineRecords(_currentPage, DEFAULT_ROWS);
             });
         },
         methods: {
-            _listMachineRecords: function (_page, _rows) {
+            _listMachineRecords: function(_page, _rows) {
 
                 vc.component.machineRecordManageInfo.conditions.page = _page;
                 vc.component.machineRecordManageInfo.conditions.row = _rows;
@@ -46,9 +46,9 @@
                 };
 
                 //发送get请求
-                vc.http.apiGet('/machineRecord.listMachineRecords"',
+                vc.http.apiGet('/machineRecord.listMachineRecords',
                     param,
-                    function (json, res) {
+                    function(json, res) {
                         var _machineRecordManageInfo = JSON.parse(json);
                         vc.component.machineRecordManageInfo.total = _machineRecordManageInfo.total;
                         vc.component.machineRecordManageInfo.records = _machineRecordManageInfo.records;
@@ -58,18 +58,19 @@
                             dataCount: vc.component.machineRecordManageInfo.total,
                             currentPage: _page
                         });
-                    }, function (errInfo, error) {
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                     }
                 );
             },
-            _queryMachineRecordMethod: function () {
+            _queryMachineRecordMethod: function() {
                 vc.component._listMachineRecords(DEFAULT_PAGE, DEFAULT_ROWS);
             },
-            _openMachineRecordDetailModel:function(_machineRecord){
-                vc.emit('machineRecordDetail', 'openMachineRecordDetailModal',_machineRecord);
+            _openMachineRecordDetailModel: function(_machineRecord) {
+                vc.emit('machineRecordDetail', 'openMachineRecordDetailModal', _machineRecord);
             },
-            _moreCondition: function () {
+            _moreCondition: function() {
                 if (vc.component.machineRecordManageInfo.moreCondition) {
                     vc.component.machineRecordManageInfo.moreCondition = false;
                 } else {
