7f3863e2dc1ddebe0d708808506efb0b92d741bf
wuxw <928255095@qq.com>
2022-10-04 18:11:00 +0800

    巡检设备

--- public/components/property/machineSelect2/machineSelect2.js
+++ public/components/property/machineSelect2/machineSelect2.js
@@ -65,7 +65,7 @@
                         return markup;
                     }, // 自定义格式化防止xss注入
                     ajax: {
-                        url: "/callComponent/machineSelect2/list",
+                        url: "/app/machine.listMachines",
                         dataType: 'json',
                         delay: 250,
                         headers:{
